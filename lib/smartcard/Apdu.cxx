439ca3e2b01b81321c9f4f5a3ed7be8bc072e37b
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-08 16:34:47 -0600

    Implement basic command apdu.

--- lib/smartcard/Apdu.cxx
+++ lib/smartcard/Apdu.cxx
@@ -1,4 +1,66 @@
 
 #include "Apdu.hxx"
 
+#include <stdexcept>
+
 using namespace smartcard;
+
+ApduCommand::ApduCommand(std::byte cla, std::byte ins, std::byte p1, std::byte p2):
+    ApduCommand(cla, ins, p1, p2, {})
+{
+}
+
+ApduCommand::ApduCommand(std::byte cla, std::byte ins, std::byte p1, std::byte p2, std::vector<std::byte> data)
+{
+    // TODO: this doesn't account for all the crazy combinations, so must be updated to handle them.
+    m_payload.resize(4 + data.size());
+    m_payload[0] = cla;
+    m_payload[1] = ins;
+    m_payload[2] = p1;
+    m_payload[3] = p2;
+    m_payload.insert(std::next(std::cbegin(m_payload), 4), std::cbegin(data), std::cend(data));
+    m_data = gsl::span<std::byte>{ std::next(std::data(m_payload), 4), std::size(data) };
+}
+
+const std::byte
+ApduCommand::Cla() const
+{
+    return m_payload[0];
+}
+
+const std::byte
+ApduCommand::Ins() const
+{
+    return m_payload[1];
+}
+
+const std::byte
+ApduCommand::P1() const
+{
+    return m_payload[2];
+}
+
+const std::byte
+ApduCommand::P2() const
+{
+    return m_payload[3];
+}
+
+gsl::span<std::byte> ApduCommand::Data() const
+{
+    return m_data;
+}
+
+/* static */
+ApduCommand
+ApduCommand::FromVector(const std::vector<std::byte>& buffer)
+{
+    throw std::logic_error("not implemented");
+}
+
+/* static */
+ApduResponse
+ApduResponse::FromVector(const std::vector<std::byte>& buffer)
+{
+    throw std::logic_error("not implemented");
+}
--- lib/smartcard/Apdu.hxx
+++ lib/smartcard/Apdu.hxx
@@ -2,15 +2,39 @@
 #ifndef __APDU_HXX__
 #define __APDU_HXX__
 
+#include <cstddef>
+#include <cstdint>
+#include <vector>
+
+#include <gsl/span>
+
 namespace smartcard
 {
 
 class ApduCommand
 {
+public:
+    ApduCommand(std::byte cla, std::byte ins, std::byte p1, std::byte p2);
+    ApduCommand(std::byte cla, std::byte ins, std::byte p1, std::byte p2, std::vector<std::byte> data);
+
+    const std::byte Cla() const;
+    const std::byte Ins() const;
+    const std::byte P1() const;
+    const std::byte P2() const;
+    gsl::span<std::byte> Data() const;
+    
+public:
+    static ApduCommand FromVector(const std::vector<std::byte>& buffer);
+
+private:
+    gsl::span<std::byte> m_data;
+    std::vector<std::byte> m_payload;
 };
 
 class ApduResponse
 {
+public:
+    static ApduResponse FromVector(const std::vector<std::byte>& buffer);
 };
 
 } // namespace smartcard
