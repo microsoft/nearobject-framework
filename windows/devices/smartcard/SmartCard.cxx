baeae6cf477739bc9bd78585f37302c303bb5ea9
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-08 14:09:53 -0600

    Add scaffold impl for windows smartcard ddi wrapper.

--- windows/devices/smartcard/SmartCard.cxx
+++ windows/devices/smartcard/SmartCard.cxx
@@ -1,4 +1,44 @@
 
 #include "SmartCard.hxx"
 
+#include <stdexcept>
+
 using namespace windows::devices;
+
+void
+Smartcard::Transmit(const smartcard::ApduCommand& command, smartcard::ApduResponse& response, std::chrono::milliseconds timeout)
+{
+    // TODO: Send IOCTL_SMARTCARD_TRANSMIT to underlying device and receive response.
+    throw std::logic_error("not implemented");
+}
+
+smartcard::ISmartcard::TransmitAsyncRequest
+Smartcard::TransmitAsync(const smartcard::ApduCommand& command, std::chrono::milliseconds timeout)
+{
+    static constexpr bool SubmissionSucceeded = true;
+    static constexpr bool SubmissionFailed = false;
+
+    const auto pendingTransmitLock = std::scoped_lock(m_pendingTxGate);
+    if (m_pendingTxResponsePromise.has_value()) {
+        // only a single asynchronous transmission is currently allowed, bail out
+        return { SubmissionFailed, {} };
+    }
+
+    // TODO: Set up asynchronous i/o with DeviceIoControl() and start it
+
+    // Create a new promise that will signal the response is available.
+    m_pendingTxResponsePromise.emplace();
+    return { SubmissionSucceeded, m_pendingTxResponsePromise->get_future() };
+}
+
+void
+Smartcard::CompletePendingTransmission(smartcard::ApduResponse response)
+{
+    const auto pendingTransmitLock = std::scoped_lock(m_pendingTxGate);
+    if (!m_pendingTxResponsePromise.has_value()) {
+        return;
+    }
+
+    m_pendingTxResponsePromise->set_value(std::move(response));
+    m_pendingTxResponsePromise.reset();
+}
--- windows/devices/smartcard/SmartCard.hxx
+++ windows/devices/smartcard/SmartCard.hxx
@@ -2,7 +2,13 @@
 #ifndef __WINDOWS_DEVICE_SMARTCARD_HXX__
 #define __WINDOWS_DEVICE_SMARTCARD_HXX__
 
+#include <chrono>
+#include <mutex>
+#include <optional>
+#include <future>
 #include <vector>
+
+#include <smartcard/ISmartCard.hxx>
 
 namespace windows
 {
@@ -13,8 +19,46 @@ namespace devices
  * @brief Helper class to interact with Windows SmartCards using the Windows SmartCard DDI:
  * https://msdn.microsoft.com/en-us/library/windows/hardware/dn905601(v=vs.85).aspx
  */
-class Smartcard
+class Smartcard :
+    public smartcard::ISmartcard
 {
+public:
+    /**
+     * @brief Synchronously transmits a command apdu and provides the response.
+     * This must handle any fragmentation of both transmitting the command and
+     * receiving the response.
+     *
+     * @param command The command apdu to send.
+     * @param response The response apdu to populate
+     * @param timeout The timeout period after which the tranmission should be canceled.
+     */
+    void
+    Transmit(const smartcard::ApduCommand& command, smartcard::ApduResponse& response, std::chrono::milliseconds timeout) override;
+
+    /**
+     * @brief Asynchronously transmits a command apdu and provides a future for
+     * the response.
+     * 
+     * @param command The command apdu to send.
+     * @param timeout The timeout period after which the tranmission should be canceled.
+     * @return smartcard::ISmartcard::TransmitAsyncRequest 
+     */
+    smartcard::ISmartcard::TransmitAsyncRequest
+    TransmitAsync(const smartcard::ApduCommand& command, std::chrono::milliseconds timeout) override;
+
+private:
+    /**
+     * @brief Construct a new Complete Pending Transmission object
+     * 
+     * @param response 
+     */
+    void CompletePendingTransmission(smartcard::ApduResponse response);
+
+private:
+    // Mutex protecting resources for the pending transmission (tx), if any.
+    // This includes reading from and writing to m_pendingTxResponsePromise.
+    std::mutex m_pendingTxGate;
+    std::optional<std::promise<smartcard::ApduResponse>> m_pendingTxResponsePromise;
 };
 
 } // namespace devices
