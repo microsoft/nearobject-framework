9993ebd0e6195147355a8d4f404bedb20d4f02aa
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-25 15:22:56 -0600

    Add session pointer verification.

--- tests/TestNearObjectSession.cxx
+++ tests/TestNearObjectSession.cxx
@@ -5,24 +5,44 @@
 #include <nearobject/NearObjectSession.hxx>
 #include <nearobject/NearObjectSessionEventCallbacks.hxx>
 
-TEST_CASE("Handlers can be registered", "[basic]")
+TEST_CASE("event handlers can be registered", "[basic]")
 {
     using namespace nearobject;
 
-    struct NearObjectSessionEventCallbacksForTest :
-        public NearObjectSessionEventCallbacks
+    struct NearObjectSessionEventCallbacksNoop :
+        public NearObjectSessionEventCallbacks 
     {
         void
-        OnNearObjectSessionEnded(NearObjectSession *session) override
-        {
-            // TODO
-        }
+        OnNearObjectSessionEnded(NearObjectSession *) override
+        {}
     };
 
     NearObjectSession session{};
-    auto callbacks = std::make_shared<NearObjectSessionEventCallbacksForTest>();
+    const auto callbacks = std::make_shared<NearObjectSessionEventCallbacksNoop>();
 
-    SECTION("registering a handler doesn't cause a crash") {
+    SECTION("registering a handler doesn't cause a crash")
+    {
+        session.RegisterCallbacks(callbacks);
+    }
+
+    SECTION("callbacks provide the session pointer for which it was registered")
+    {
+        struct NearObjectSessionEventCallbacksCheckSessionPointer :
+            public NearObjectSessionEventCallbacks {
+            NearObjectSessionEventCallbacksCheckSessionPointer(NearObjectSession *session) :
+                Session(session)
+            { }
+
+            void
+            OnNearObjectSessionEnded(NearObjectSession *session) override
+            {
+                CHECK(Session == session);
+            }
+
+            NearObjectSession *Session;
+        };
+
+        const auto callbacks = std::make_shared<NearObjectSessionEventCallbacksCheckSessionPointer>(&session);
         session.RegisterCallbacks(callbacks);
     }
 }
