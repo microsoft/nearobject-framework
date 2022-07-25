6c4541d002b36e0872152217f9710a4d25c5e91f
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-25 11:41:59 -0600

    Add catch2-based unit test structure.

---
+++ tests/README.md
@@ -0,0 +1,3 @@
+# Near Object Framework Tests
+
+Presently, the [Catch2](https://github.com/catchorg/Catch2) open-source library is used for unit testing. Catch2 uses the BSL 1.0 license, which is very permissive.
---
+++ tests/TestNearObjectSession.cxx
@@ -0,0 +1,28 @@
+
+#include <catch2/catch.hpp>
+#include <memory>
+
+#include <nearobject/NearObjectSession.hxx>
+#include <nearobject/NearObjectSessionEventCallbacks.hxx>
+
+TEST_CASE("Handlers can be reigstered", "[basic]")
+{
+    using namespace nearobject;
+
+    struct NearObjectSessionEventCallbacksForTest :
+        public NearObjectSessionEventCallbacks
+    {
+        void
+        OnNearObjectSessionEnded(NearObjectSession *session) override
+        {
+            // TODO
+        }
+    };
+
+    NearObjectSession session{};
+    auto callbacks = std::make_shared<NearObjectSessionEventCallbacksForTest>();
+
+    SECTION("registering a handler doesn't cause a crash") {
+        session.RegisterCallbacks(callbacks);
+    }
+}
