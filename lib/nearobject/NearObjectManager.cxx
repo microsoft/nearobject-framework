7aee27a58ad95d6e7ced403fcae5aa9142c17f23
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-26 17:17:46 -0600

    Add a few api functions for the nearobjectmanager.

--- lib/nearobject/NearObjectManager.cxx
+++ lib/nearobject/NearObjectManager.cxx
@@ -2,3 +2,18 @@
 #include "NearObjectManager.hxx"
 
 using namespace nearobject;
+
+std::vector<NearObjectConnectionProfile>
+NearObjectManager::FindAllConnectionProfiles() const
+{
+    // TODO: proxy to service ConnectionProfileManager, or, use it directly
+    // here with an shared instance
+    return {};
+}
+
+NearObjectManager::NearObjectConnectionResult
+NearObjectManager::EstablishConnection(const NearObjectConnectionProfile& connectionProfile)
+{
+    // TODO: proxy to valid NearObjectDeviceManager
+    return {};
+}
--- lib/nearobject/NearObjectManager.hxx
+++ lib/nearobject/NearObjectManager.hxx
@@ -2,6 +2,8 @@
 #ifndef __NEAR_OBJECT_MANAGER_HXX__
 #define __NEAR_OBJECT_MANAGER_HXX__
 
+#include <vector>
+
 #include <nearobject/NearObjectConnectionProfile.hxx>
 
 namespace nearobject
@@ -9,13 +11,31 @@ namespace nearobject
 /**
  * @brief Central class managing the use of Near Object services. This class
  * exposes the primary API of the framework.
- * 
+ *
  * This class ties together all of the different framework components.
  */
 class NearObjectManager
 {
 public:
+    struct NearObjectConnectionResult {
+    };
 
+    /**
+     * @brief Attempt to establish a near object connection using the specified profile. 
+     * 
+     * @param connectionProfile 
+     * @return NearObjectConnectionResult 
+     */
+    NearObjectConnectionResult
+    EstablishConnection(const NearObjectConnectionProfile& connectionProfile);
+
+    /**
+     * @brief Find and enumerate all known connection profiles. 
+     * 
+     * @return std::vector<NearObjectConnectionProfile> 
+     */
+    std::vector<NearObjectConnectionProfile>
+    FindAllConnectionProfiles() const;
 };
 } // namespace nearobject
 
