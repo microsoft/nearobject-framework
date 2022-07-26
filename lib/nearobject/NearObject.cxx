edcc58e7feb0147930e1c8ad2307d7dfc955c136
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-26 14:03:01 -0600

    Add NO equality function.

--- lib/nearobject/NearObject.cxx
+++ lib/nearobject/NearObject.cxx
@@ -1,2 +1,17 @@
 
 #include "NearObject.hxx"
+
+using namespace nearobject;
+
+bool
+NearObject::IsSame(const NearObject& other) const noexcept
+{
+    // TODO: update this once this class has data members.
+    return (this == &other);
+}
+
+bool
+nearobject::operator==(const NearObject& lhs, const NearObject& rhs) noexcept
+{
+    return lhs.IsSame(rhs);
+}
--- lib/nearobject/NearObject.hxx
+++ lib/nearobject/NearObject.hxx
@@ -9,7 +9,21 @@ namespace nearobject
  */
 class NearObject
 {
+public:
+    /**
+     * @brief Determines if the specified near object refers to the same entity
+     * as this near object. 
+     * 
+     * @param other
+     * @return true 
+     * @return false 
+     */
+    bool
+    IsSame(const NearObject& other) const noexcept;
 };
+
+bool
+operator==(const NearObject&, const NearObject&) noexcept;
 
 } // namespace nearobject
 
