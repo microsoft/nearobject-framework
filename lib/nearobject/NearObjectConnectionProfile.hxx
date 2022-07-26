564cd455586a70da2283b9d6494c19272971fa64
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-26 16:46:34 -0600

    Add some basic properties to connection profiles.

--- lib/nearobject/NearObjectConnectionProfile.hxx
+++ lib/nearobject/NearObjectConnectionProfile.hxx
@@ -1,13 +1,25 @@
 
-#ifndef __CONNECTION_PROFILE_HXX__
-#define __CONNECTION_PROFILE_HXX__
+#ifndef __NEAR_OBJECT_CONNECTION_PROFILE_HXX__
+#define __NEAR_OBJECT_CONNECTION_PROFILE_HXX__
+
+#include <optional>
+
+#include "NearObjectConnectionProfileSecurity.hxx"
 
 namespace nearobject
 {
-class NearObjectConnectionProfile
+enum NearObjectConnectionScope
 {
+    Unicast,
+    Multicast,
+};
+
+struct NearObjectConnectionProfile
+{
+    NearObjectConnectionScope Scope { NearObjectConnectionScope::Unicast };
+    std::optional<NearObjectConnectionProfileSecurity> Security{ std::nullopt };
 };
 
 } // namespace nearobject
 
-#endif // __CONNECTION_PROFILE_HXX__
+#endif // __NEAR_OBJECT_CONNECTION_PROFILE_HXX__
---
+++ lib/nearobject/NearObjectConnectionProfileSecurity.hxx
@@ -0,0 +1,13 @@
+
+#ifndef __NEAR_OBJECT_CONNECTION_PROFILE_SECURITY_HXX__
+#define __NEAR_OBJECT_CONNECTION_PROFILE_SECURITY_HXX__
+
+namespace nearobject
+{
+struct NearObjectConnectionProfileSecurity 
+{
+    virtual ~NearObjectConnectionProfileSecurity() = default;
+};
+} // namespace nearobject
+
+#endif // __NEAR_OBJECT_CONNECTION_PROFILE_SECURITY_HXX__
