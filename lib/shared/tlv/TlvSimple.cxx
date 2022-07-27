a6205fe5ae06ed36e7d40c9d59b529169fbe02e3
Shane (the Bug Conqueror) Guan <shaneguan@microsoft.com>
2022-07-27 14:55:57 -0700

    typo

--- lib/shared/tlv/TlvSimple.cxx
+++ lib/shared/tlv/TlvSimple.cxx
@@ -5,7 +5,7 @@ using namespace encoding;
 
 /* static */
 Tlv::ParseResult
-Tlv::Parse(std::unique_ptr<Tlv>* tlvOutput, gsl::span<std::byte> /* data */)
+TlvSimple::Parse(std::unique_ptr<Tlv>* tlvOutput, gsl::span<std::byte> /* data */)
 {
     std::unique_ptr<Tlv> tlvParsed{ nullptr };
     Tlv::ParseResult parseResult = Tlv::ParseResult::Failed;
