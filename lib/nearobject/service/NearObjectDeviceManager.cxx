0f45ca3099d4c19bd86441f1caaa0ce79778ab61
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-26 19:50:45 -0600

    Expose NearObjectDevices from the Manager.

--- lib/nearobject/service/NearObjectDeviceManager.cxx
+++ lib/nearobject/service/NearObjectDeviceManager.cxx
@@ -9,8 +9,8 @@ using namespace nearobject::service;
 void
 NearObjectDeviceManager::AddDevice(std::shared_ptr<NearObjectDevice> nearObjectDevice)
 {
-    auto nearObjectDevicesLock = std::scoped_lock{ m_nearObjectDeviceGate };
-    auto nearObjectDeviceExists = std::any_of(std::cbegin(m_nearObjectDevices), std::cend(m_nearObjectDevices), [&](const auto& nearObjectDeviceExisting) {
+    const auto nearObjectDevicesLock = std::scoped_lock{ m_nearObjectDeviceGate };
+    const auto nearObjectDeviceExists = std::any_of(std::cbegin(m_nearObjectDevices), std::cend(m_nearObjectDevices), [&](const auto& nearObjectDeviceExisting) {
         return /* TODO */ false;
     });
 
@@ -19,4 +19,29 @@ NearObjectDeviceManager::AddDevice(std::shared_ptr<NearObjectDevice> nearObjectD
     }
 
     m_nearObjectDevices.push_back(std::move(nearObjectDevice));
+}
+
+std::shared_ptr<NearObjectDevice>
+NearObjectDeviceManager::GetDefaultDevice() const
+{
+    const auto nearObjectDevicesLock = std::scoped_lock{ m_nearObjectDeviceGate };
+
+    return m_nearObjectDevices.empty() 
+        ? nullptr
+        : m_nearObjectDevices.front(); 
+}
+
+std::vector<std::weak_ptr<NearObjectDevice>>
+NearObjectDeviceManager::GetAllDevices() const
+{
+    const auto nearObjectDevicesLock = std::scoped_lock{ m_nearObjectDeviceGate };
+
+    std::vector<std::weak_ptr<NearObjectDevice>> nearObjectDevices{};
+    std::transform(std::cbegin(m_nearObjectDevices), std::cend(m_nearObjectDevices), std::back_inserter(nearObjectDevices), [](const auto& nearObjectDevice)
+    {
+        // Implicit conversion from std::shared_ptr to std::weak_ptr.
+        return nearObjectDevice;
+    });
+
+    return nearObjectDevices;
 }
--- lib/nearobject/service/NearObjectDeviceManager.hxx
+++ lib/nearobject/service/NearObjectDeviceManager.hxx
@@ -12,16 +12,54 @@ namespace service
 {
 class NearObjectDevice;
 
+/**
+ * @brief Manages devices which implement the NearObjectDevice interface,
+ * thereby providing Near Object services.
+ * 
+ * This class is responsible for tracking the presence of such devices, both
+ * when new devices ae discovered and when existing devices are removed or
+ * become otherwise unavailable.
+ */
 class NearObjectDeviceManager
 {
     /**
-     * @brief Adds a new device for monitoring.
+     * @brief Adds a new device for use by the framework to implement Near
+     * Object services.
+     * 
+     * @param nearObjectDevice The device to add.
      */
     void
     AddDevice(std::shared_ptr<NearObjectDevice> nearObjectDevice);
 
+    /**
+     * @brief Get the default near object device.
+     * 
+     * In most cases, this is sufficient to carry out basic near object
+     * services. If a device with specific capabilities is required, the
+     * GetAllDevices() function can be used to obtain the complete collection of
+     * known devices and filter them appropriately.
+     * 
+     * @return std::shared_ptr<NearObjectDevice> 
+     */
+    std::shared_ptr<NearObjectDevice>
+    GetDefaultDevice() const;
+
+    /**
+     * @brief Get a collection of all near object devices.
+     * 
+     * This function allows the caller to further filter the collection based
+     * on it's own set of conditions. As such, weak_ptrs are provided since 
+     * no assumptions are made as to when the caller will actually filter
+     * the collection and select a subset for use, thereby promoting the
+     * weak pointers to shared pointers.
+     * 
+     * @return std::vector<std::weak_ptr<NearObjectDevice>> 
+     */
+    std::vector<std::weak_ptr<NearObjectDevice>>
+    GetAllDevices() const;
+
 private:
-    std::mutex m_nearObjectDeviceGate;
+    mutable std::mutex m_nearObjectDeviceGate;
     std::vector<std::shared_ptr<NearObjectDevice>> m_nearObjectDevices{};
 };
 
