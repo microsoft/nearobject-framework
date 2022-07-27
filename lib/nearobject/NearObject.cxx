9d027f75b97c8d63f09bd101ff7b29892c4ffb4f
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-27 11:15:28 -0600

    Expose spatial properties from NearObject.

--- lib/nearobject/NearObject.cxx
+++ lib/nearobject/NearObject.cxx
@@ -1,7 +1,16 @@
 
+#include <tuple>
+
 #include "NearObject.hxx"
 
 using namespace nearobject;
+
+NearObjectSpatialProperties
+NearObject::GetSpatialProperties() const noexcept
+{
+    const auto lock = std::scoped_lock{ m_spatialPropertiesGate };
+    return m_spatialProperties;
+}
 
 bool
 NearObject::IsSame(const NearObject& other) const noexcept
@@ -14,4 +23,11 @@ bool
 nearobject::operator==(const NearObject& lhs, const NearObject& rhs) noexcept
 {
     return lhs.IsSame(rhs);
+}
+
+bool
+nearobject::operator==(const NearObjectSpatialProperties& lhs, const NearObjectSpatialProperties& rhs) noexcept
+{
+    return std::tie(lhs.Distance, lhs.AngleAzimuth, lhs.AngleElevation, lhs.Elevation)
+        == std::tie(rhs.Distance, rhs.AngleAzimuth, rhs.AngleElevation, rhs.Elevation);
 }
--- lib/nearobject/NearObject.hxx
+++ lib/nearobject/NearObject.hxx
@@ -2,14 +2,43 @@
 #ifndef __NEAR_OBJECT_HXX__
 #define __NEAR_OBJECT_HXX__
 
+#include <mutex>
+#include <optional>
+
 namespace nearobject
 {
+/**
+ * @brief Decribes the spatial properties of a near object. All property values
+ * are relative to the hosting device.
+ * 
+ * TODO: Select and document coordinate system
+ * TODO: Select and document units
+ */
+struct NearObjectSpatialProperties 
+{
+    std::optional<double> Distance;
+    std::optional<double> AngleAzimuth;
+    std::optional<double> AngleElevation;
+    std::optional<double> Elevation;
+};
+
+bool
+operator==(const NearObjectSpatialProperties&, const NearObjectSpatialProperties&) noexcept;
+
 /**
  * @brief Represents a near object.
  */
 class NearObject
 {
 public:
+    /**
+     * @brief Retrieves the latest spatial properties for these near object.
+     * 
+     * @return NearObjectSpatialProperties 
+     */
+    NearObjectSpatialProperties
+    GetSpatialProperties() const noexcept;
+
     /**
      * @brief Determines if the specified near object refers to the same entity
      * as this near object. 
@@ -20,6 +49,10 @@ public:
      */
     bool
     IsSame(const NearObject& other) const noexcept;
+
+private:
+    mutable std::mutex m_spatialPropertiesGate;
+    NearObjectSpatialProperties m_spatialProperties{};
 };
 
 bool
