bcfb8f58277f058973a693b1f24621258f5efb3f
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-28 17:43:43 -0600

    Add enumeration function for profiles.

--- lib/nearobject/service/NearObjectProfileManager.hxx
+++ lib/nearobject/service/NearObjectProfileManager.hxx
@@ -2,6 +2,7 @@
 #ifndef __NEAR_OBJECT_PROFILE_MANAGER_HXX__
 #define __NEAR_OBJECT_PROFILE_MANAGER_HXX__
 
+#include <shared_mutex>
 #include <vector>
 
 #include <nearobject/NearObjectProfile.hxx>
@@ -13,8 +14,65 @@ namespace service
 class NearObjectProfileManager
 {
 public:
+    /**
+     * @brief Describes the lifetime of the profile.
+     */
+    enum ProfileLifetime {
+        Persistent,
+        Ephemeral
+    };
+
+    /**
+     * @brief Adds a new profile to support.
+     *
+     * @param profile The profile to add.
+     * @param lifetime The lifetime of the profile.
+     */
+    void
+    AddProfile(const NearObjectProfile& profile, ProfileLifetime lifetime = ProfileLifetime::Persistent);
+
+    /**
+     * @brief Find all profiles which match the provided profile.
+     *
+     * TODO: Matching by another profile might not make sense. Consider what
+     * input argument(s) are more sensible, and update this prototype with them.
+     * This will likely come from an out-of-band connection, and may identify
+     * this device as a peer in some way.
+     *
+     * @param profile
+     * @return std::vector<NearObjectProfile>
+     */
     std::vector<NearObjectProfile>
-    FindMatchingProfiles(const NearObjectProfile& connectionProfile) const;
+    FindMatchingProfiles(const NearObjectProfile& profile) const;
+
+    /**
+     * @brief Return all known profiles.
+     *
+     * @return std::vector<NearObjectProfile>
+     */
+    std::vector<NearObjectProfile>
+    GetAllProfiles() const;
+
+protected:
+    /**
+     * @brief Persist the profile.
+     *
+     * @param profile The profile to persist.
+     */
+    void
+    PersistProfile(const NearObjectProfile& profile);
+
+    /**
+     * @brief Obtain all persisted profiles.
+     *
+     * @return std::vector<NearObjectProfile>
+     */
+    std::vector<NearObjectProfile>
+    ReadPersistedProfiles() const;
+
+private:
+    mutable std::shared_mutex m_profilesGate{};
+    std::vector<NearObjectProfile> m_profiles{};
 };
 
 } // namespace service
