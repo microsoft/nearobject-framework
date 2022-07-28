ef7046891757fb17e4312f18c8a3b23ef97c68be
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-27 20:47:13 -0600

    Add basic tests for NearObjectManager.

---
+++ tests/TestNearObjectManager.cxx
@@ -0,0 +1,44 @@
+
+#include <array>
+#include <memory>
+
+#include <catch2/catch.hpp>
+
+#include <nearobject/NearObjectManager.hxx>
+
+TEST_CASE("near object manager can be created", "[basic]")
+{
+    using namespace nearobject;
+
+    SECTION("creation doesn't cause a crash")
+    {
+        NearObjectManager manager{};
+    }
+}
+
+TEST_CASE("near object manager apis don't cause a crash")
+{
+    using namespace nearobject;
+
+    SECTION("FindAllConnectionProfiles doesn't cause a crash")
+    {
+        NearObjectManager manager{};
+        const auto connectionProfiles = manager.FindAllConnectionProfiles();
+    }
+
+    SECTION("EstablishConnection doesn't cause a crash with unknown profile")
+    {
+        NearObjectManager manager{};
+        const NearObjectConnectionProfile connectionProfile{};
+        const auto connectionResult = manager.EstablishConnection(connectionProfile);
+    }
+
+    SECTION("EstablishConnection doesn't cause a crash with any known profile")
+    {
+        NearObjectManager manager{};
+        const auto connectionProfiles = manager.FindAllConnectionProfiles();
+        for (const auto& connectionProfile : connectionProfiles) {
+            const auto connectionResult = manager.EstablishConnection(connectionProfile);
+        }
+    }
+}
