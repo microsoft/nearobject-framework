d9a067cb877465bb9ea5262c75ace65caba64f66
Andrew Beltrano <anbeltra@microsoft.com>
2022-07-31 07:40:23 -0600

    Add basic tests for ProbeAsync.

--- tests/TestNearObjectDeviceDiscoveryAgent.cxx
+++ tests/TestNearObjectDeviceDiscoveryAgent.cxx
@@ -1,7 +1,10 @@
 
+#include <algorithm>
+#include <chrono>
 #include <memory>
 
 #include <catch2/catch.hpp>
+#include <nearobject/service/NearObjectDevice.hxx>
 #include <nearobject/service/NearObjectDeviceDiscoveryAgent.hxx>
 
 namespace nearobject
@@ -31,12 +34,39 @@ protected:
         return ProbePromise.get_future();
     }
 };
+
+struct NearObjectDeviceTest :
+    public NearObjectDevice {
+
+    explicit NearObjectDeviceTest(uint64_t deviceId) :
+        NearObjectDevice(deviceId)
+    {}
+
+    StartSessionResult
+    StartSessionImpl(const NearObjectProfile& profile, std::weak_ptr<NearObjectSessionEventCallbacks> eventCallbacks) override
+    {
+        return { std::nullopt };   
+    }
+};
 } // namespace test
 } // namespace service
 } // namespace nearobject
 
+namespace std
+{
+template <>
+struct equal_to<std::weak_ptr<nearobject::service::test::NearObjectDeviceTest>>
+{
+    bool operator()(const std::weak_ptr<nearobject::service::test::NearObjectDeviceTest>& lhs, const std::weak_ptr<nearobject::service::test::NearObjectDeviceTest>& rhs) const noexcept
+    {
+        return *(lhs.lock()) == *(rhs.lock());
+    }
+};
+} // namespace std
+
 TEST_CASE("near object device discovery agent can be created", "[basic][service]")
 {
+    using namespace std::chrono_literals;
     using namespace nearobject::service;
 
     test::NearObjectDeviceDiscoveryAgentTest discoveryAgentTest{};
@@ -70,20 +100,56 @@ TEST_CASE("near object device discovery agent can be created", "[basic][service]
         REQUIRE(discoveryAgentTest.IsStarted());
     }
 
-    SECTION("discovery event callback can be reigstered without causing a crash")
+    SECTION("discovery event callback can be registered without causing a crash")
     {
-        discoveryAgentTest.RegisterDiscoveryEventCallback([](auto&&, auto&&){});
+        discoveryAgentTest.RegisterDiscoveryEventCallback([](auto&&, auto&&) {});
     }
 
     SECTION("discovery event callback can be registered in started state")
     {
         discoveryAgentTest.Start();
-        discoveryAgentTest.RegisterDiscoveryEventCallback([](auto&&, auto&&){});
+        discoveryAgentTest.RegisterDiscoveryEventCallback([](auto&&, auto&&) {});
     }
 
     SECTION("discovery event callback can be registered in stopped state")
     {
         discoveryAgentTest.Stop();
-        discoveryAgentTest.RegisterDiscoveryEventCallback([](auto&&, auto&&){});
+        discoveryAgentTest.RegisterDiscoveryEventCallback([](auto&&, auto&&) {});
+    }
+
+    SECTION("discovery probe promise is eventually completed")
+    {
+        static constexpr auto probeTimeout = 1ms;
+
+        std::vector<std::shared_ptr<NearObjectDevice>> probeDevices{
+            std::make_shared<test::NearObjectDeviceTest>(0x1),
+            std::make_shared<test::NearObjectDeviceTest>(0x2),
+            std::make_shared<test::NearObjectDeviceTest>(0x3),
+        };
+
+        // Kick of an async probe.
+        auto probeFuture = discoveryAgentTest.ProbeAsync();
+        REQUIRE(probeFuture.valid());
+
+        // [test] Set the value of the promise, which will update the future shared state.
+        discoveryAgentTest.ProbePromise.set_value({
+            probeDevices[0], probeDevices[1], probeDevices[2],    
+        });
+
+        // Wait for the future to complete and get the result.
+        const auto probeCompletion = probeFuture.wait_for(probeTimeout);
+        REQUIRE(probeCompletion == std::future_status::ready);
+        const auto probeResults = probeFuture.get();
+        REQUIRE(!probeFuture.valid());
+
+        // Verify the future provided the expected results.
+        std::vector<std::shared_ptr<NearObjectDevice>> probeResultsStrong{};
+        std::transform(std::cbegin(probeResults), std::cend(probeResults), std::back_inserter(probeResultsStrong), [](auto& probeDevice)
+        {
+            return probeDevice.lock();
+        });
+
+        REQUIRE(probeResults.size() == probeDevices.size());
+        REQUIRE(probeResultsStrong == probeDevices);
     }
 }
