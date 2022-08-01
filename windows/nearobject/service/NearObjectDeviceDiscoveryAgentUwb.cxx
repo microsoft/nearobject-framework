a03c5f4eb0a74f74e0695ebeaf330ee691eb2dc9
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-01 14:41:08 -0600

    Add basic discovery agent for Windows UWB devices.

--- windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.cxx
+++ windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.cxx
@@ -1,4 +1,16 @@
 
+#include <nearobject/service/NearObjectDeviceUwb.hxx>
 #include "NearObjectDeviceDiscoveryAgentUwb.hxx"
+#include "UwbDevice.hxx"
 
 using namespace windows::nearobject::service;
+
+void
+NearObjectDeviceDiscoveryAgentUwb::SignalDiscoveryEvent(::nearobject::service::NearObjectDevicePresence presence, std::unique_ptr<windows::devices::UwbDevice> uwbDevice) const noexcept
+{
+    static constexpr auto deviceIdFallback = 0x1234;
+
+    // TODO: determine how devices will be assigned unique identifiers
+    auto nearObjectDevice = std::make_shared<::nearobject::service::NearObjectDeviceUwb>(deviceIdFallback, std::move(uwbDevice));
+    DevicePresenceChanged(presence, std::move(nearObjectDevice));
+}
--- windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.hxx
+++ windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.hxx
@@ -2,10 +2,16 @@
 #ifndef NEAR_OBJECT_DEVICE_DISCOVERY_AGENT_UWB
 #define NEAR_OBJECT_DEVICE_DISCOVERY_AGENT_UWB
 
+#include <memory>
+
 #include <nearobject/service/NearObjectDeviceDiscoveryAgent.hxx>
 
 namespace windows
 {
+namespace devices
+{
+class UwbDevice;
+}
 namespace nearobject
 {
 namespace service
@@ -13,6 +19,9 @@ namespace service
 class NearObjectDeviceDiscoveryAgentUwb :
     public ::nearobject::service::NearObjectDeviceDiscoveryAgent
 {
+private:
+    void
+    SignalDiscoveryEvent(::nearobject::service::NearObjectDevicePresence presence, std::unique_ptr<windows::devices::UwbDevice> uwbDevice) const noexcept;
 };
 } // namespace service
 } // namespace nearobject
