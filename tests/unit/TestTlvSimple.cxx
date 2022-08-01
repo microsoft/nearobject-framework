81f981a037b39a033b8e11dc546266bede2a6a79
Shane (the Bug Conqueror) Guan <shaneguan@microsoft.com>
2022-08-01 16:40:42 -0700

    first simple tlv test

---
+++ tests/unit/TestTlvSimple.cxx
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <catch2/catch.hpp>
+#include <string>
+#include <shared/tlv/TlvSimple.hxx>
+
+TEST_CASE("TlvSimple object can be created properly from the Parse function", "[basic][infra]")
+{
+    SECTION("creating a TlvSimple from a small amount of data works as expected")
+    {
+        std::string mystr("hellothisisbob");
+        std::vector<std::byte> mydata_small(mystr.size());
+        for(std::size_t i=0; i<mystr.size(); i++){
+            mydata_small[i] = std::byte{(unsigned char)mystr[i]};
+        }
+
+        // create the vector for the packet
+        std::byte tag{0x3};
+        std::byte length{uint8_t(mydata_small.size())}; 
+        // std::byte length{4};
+        std::vector<std::byte> packet;
+
+        packet.insert(packet.end(),tag);
+        packet.insert(packet.end(),length);
+        packet.insert(packet.end(),mydata_small.begin(),mydata_small.end());
+
+        // try to parse
+        encoding::TlvSimple *tlvparsed = nullptr; 
+        auto result = encoding::TlvSimple::Parse(&tlvparsed, packet);
+        REQUIRE(result == encoding::Tlv::ParseResult::Succeeded);
+
+        // check that it parsed correctly
+        REQUIRE(tlvparsed != nullptr);
+        REQUIRE(tlvparsed->Tag.size()==1);
+        REQUIRE(tlvparsed->Tag[0] == tag);
+        REQUIRE(tlvparsed->Value.size() == std::size_t(length));
+        for(std::size_t i=0;i<std::size_t(length);i++)
+            REQUIRE((tlvparsed->Value[i]==mydata_small[i]));
+    }
+}
