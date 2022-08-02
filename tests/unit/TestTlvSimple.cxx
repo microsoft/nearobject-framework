c2e11274344995337888b350b730d429a45bc951
Shane (the Bug Conqueror) Guan <shaneguan@microsoft.com>
2022-08-02 14:24:09 -0700

    address comments

--- tests/unit/TestTlvSimple.cxx
+++ tests/unit/TestTlvSimple.cxx
@@ -2,15 +2,16 @@
 #include <catch2/catch.hpp>
 #include <shared/tlv/TlvSimple.hxx>
 #include <string>
+#include <vector>
 
 TEST_CASE("TlvSimple object can be created properly from the Parse function", "[basic][infra]")
 {
     SECTION("creating a TlvSimple from no data works as expected")
     {
         std::string mystr("");
-        std::vector<std::byte> mydata_small(mystr.size());
+        std::vector<std::byte> mydataSmall(mystr.size());
         for (std::size_t i = 0; i < mystr.size(); i++) {
-            mydata_small[i] = std::byte{ (unsigned char)mystr[i] };
+            mydataSmall[i] = std::byte{ (unsigned char)mystr[i] };
         }
 
         // create the vector for the packet
@@ -20,7 +21,7 @@ TEST_CASE("TlvSimple object can be created properly from the Parse function", "[
 
         packet.insert(packet.end(), tag);
         packet.insert(packet.end(), length);
-        packet.insert(packet.end(), mydata_small.begin(), mydata_small.end());
+        packet.insert(std::cend(packet), std::cbegin(mydataSmall), std::cend(mydataSmall));
 
         // try to parse
         encoding::TlvSimple *tlvparsed = nullptr;
@@ -33,25 +34,25 @@ TEST_CASE("TlvSimple object can be created properly from the Parse function", "[
         REQUIRE(tlvparsed->Tag[0] == tag);
         REQUIRE(tlvparsed->Value.size() == std::size_t(length));
         for (std::size_t i = 0; i < std::size_t(length); i++)
-            REQUIRE((tlvparsed->Value[i] == mydata_small[i]));
+            REQUIRE((tlvparsed->Value[i] == mydataSmall[i]));
     }
 
     SECTION("creating a TlvSimple from a small amount of data works as expected")
     {
         std::string mystr("hellothisisbob");
-        std::vector<std::byte> mydata_small(mystr.size());
+        std::vector<std::byte> mydataSmall(mystr.size());
         for (std::size_t i = 0; i < mystr.size(); i++) {
-            mydata_small[i] = std::byte{ (unsigned char)mystr[i] };
+            mydataSmall[i] = std::byte{ (unsigned char)mystr[i] };
         }
 
         // create the vector for the packet
         std::byte tag{ 0x3 };
-        std::byte length{ uint8_t(mydata_small.size()) };
+        std::byte length{ uint8_t(mydataSmall.size()) };
         std::vector<std::byte> packet;
 
         packet.insert(packet.end(), tag);
         packet.insert(packet.end(), length);
-        packet.insert(packet.end(), mydata_small.begin(), mydata_small.end());
+        packet.insert(std::cend(packet), std::cbegin(mydataSmall), std::cend(mydataSmall));
 
         // try to parse
         encoding::TlvSimple *tlvparsed = nullptr;
@@ -64,25 +65,25 @@ TEST_CASE("TlvSimple object can be created properly from the Parse function", "[
         REQUIRE(tlvparsed->Tag[0] == tag);
         REQUIRE(tlvparsed->Value.size() == std::size_t(length));
         for (std::size_t i = 0; i < std::size_t(length); i++)
-            REQUIRE((tlvparsed->Value[i] == mydata_small[i]));
+            REQUIRE((tlvparsed->Value[i] == mydataSmall[i]));
     }
 
     SECTION("creating a TlvSimple from a incorrect small amount of data fails as expected")
     {
         std::string mystr("hellothisisbob");
-        std::vector<std::byte> mydata_small(mystr.size());
+        std::vector<std::byte> mydataSmall(mystr.size());
         for (std::size_t i = 0; i < mystr.size(); i++) {
-            mydata_small[i] = std::byte{ (unsigned char)mystr[i] };
+            mydataSmall[i] = std::byte{ (unsigned char)mystr[i] };
         }
 
         // create the vector for the packet
         std::byte tag{ 0x3 };
-        std::byte length{ uint8_t(mydata_small.size() + 1) };
+        std::byte length{ uint8_t(mydataSmall.size() + 1) };
         std::vector<std::byte> packet;
 
         packet.insert(packet.end(), tag);
         packet.insert(packet.end(), length);
-        packet.insert(packet.end(), mydata_small.begin(), mydata_small.end());
+        packet.insert(std::cend(packet), std::cbegin(mydataSmall), std::cend(mydataSmall));
 
         // try to parse
         encoding::TlvSimple *tlvparsed = nullptr;
