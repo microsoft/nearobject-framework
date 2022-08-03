97c1c842bd0c6a03aa716910d75fb5ec31afaa1a
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-03 00:16:30 -0600

    Add initial device detection logic for Windows UWB devices.

--- windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.cxx
+++ windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.cxx
@@ -1,25 +1,112 @@
 
+#include <string>
+
 #include "NearObjectDeviceDiscoveryAgentUwb.hxx"
 
-#include <nearobject/service/NearObjectDeviceUwb.hxx>
 #include <UwbDevice.hxx>
+#include <UwbDeviceDriver.hxx>
+#include <nearobject/service/NearObjectDeviceUwb.hxx>
 
 using namespace windows::nearobject::service;
+using ::nearobject::service::NearObjectDevice;
 using ::nearobject::service::NearObjectDevicePresence;
+using ::nearobject::service::NearObjectDeviceUwb;
 
-std::shared_ptr<::nearobject::service::NearObjectDeviceUwb>
-NearObjectDeviceDiscoveryAgentUwb::SignalDeviceAdded(std::unique_ptr<windows::devices::UwbDevice> uwbDevice) const noexcept
+std::shared_ptr<NearObjectDeviceUwb>
+NearObjectDeviceDiscoveryAgentUwb::AddCachedUwbNearObjectDevice(const std::wstring &deviceName)
 {
-    static constexpr auto deviceIdFallback = 0x1234;
+    // Note: the exclusive lock must be held for the duration, even during the
+    // cache lookup parts to prevent races with concurrent notifications adding
+    // entries to the cache.
+    const auto nearObjectDeviceLockExclusive = std::scoped_lock{ m_nearObjectDeviceCacheGate };
 
-    // TODO: determine how devices will be assigned unique identifiers
-    auto nearObjectDeviceUwb = std::make_shared<::nearobject::service::NearObjectDeviceUwb>(deviceIdFallback, std::move(uwbDevice));
-    DevicePresenceChanged(NearObjectDevicePresence::Arrived, nearObjectDeviceUwb);
-    return nearObjectDeviceUwb;
+    // Attempt to find a cached device instance.
+    const auto nearObjectDeviceCacheNode = m_nearObjectDeviceCache.find(deviceName);
+    if (nearObjectDeviceCacheNode != std::cend(m_nearObjectDeviceCache)) {
+        // Cached instance found; attempt to promote it to shared_ptr.
+        auto nearObjectDeviceWeak = nearObjectDeviceCacheNode->second;
+        auto nearObjectDevice = nearObjectDeviceWeak.lock();
+        if (nearObjectDevice != nullptr) {
+            return nearObjectDevice;
+        }
+        // Cached instance is no longer valid. Remove it, then fall-through to
+        // below to create and add a new instance.
+        m_nearObjectDeviceCache.erase(nearObjectDeviceCacheNode);
+    }
+
+    // Create a new instance and add it to the cache.
+    auto uwbDevice = std::make_unique<windows::devices::UwbDevice>(deviceName);
+    auto nearObjectDevice = std::make_shared<NearObjectDeviceUwb>(std::move(uwbDevice));
+    m_nearObjectDeviceCache[deviceName] = nearObjectDevice;
+
+    return nearObjectDevice;
+}
+
+std::shared_ptr<NearObjectDeviceUwb>
+NearObjectDeviceDiscoveryAgentUwb::ExtractCachedNearObjectDevice(const std::wstring &deviceName)
+{
+    auto nearObjectDeviceLockExclusive = std::unique_lock{ m_nearObjectDeviceCacheGate };
+    auto nearObjectExtractResult = m_nearObjectDeviceCache.extract(deviceName);
+    return nearObjectExtractResult.empty()
+        ? nullptr
+        : nearObjectExtractResult.mapped().lock();
 }
 
 void
-NearObjectDeviceDiscoveryAgentUwb::SignalDeviceRemoved(std::shared_ptr<::nearobject::service::NearObjectDeviceUwb> nearObjectDeviceUwb) const noexcept
+NearObjectDeviceDiscoveryAgentUwb::RegisterForUwbDeviceClassNotifications()
 {
-    DevicePresenceChanged(NearObjectDevicePresence::Departed, std::move(nearObjectDeviceUwb));
+    CM_NOTIFY_FILTER interfaceFilter{};
+    interfaceFilter.cbSize = sizeof interfaceFilter;
+    interfaceFilter.FilterType = CM_NOTIFY_FILTER_TYPE_DEVICEINTERFACE;
+    interfaceFilter.u.DeviceInterface.ClassGuid = windows::devices::uwb::InterfaceClassUwb;
+
+    auto notifyRegistrationResult = CM_Register_Notification(&interfaceFilter, this, OnDeviceInterfaceNotificationCallback, &m_uwbHcmNotificationHandle);
+    if (notifyRegistrationResult != CR_SUCCESS) {
+        // TODO: log? throw?
+    }
+}
+
+void
+NearObjectDeviceDiscoveryAgentUwb::OnDeviceInterfaceNotification(HCMNOTIFICATION hcmNotificationHandle, CM_NOTIFY_ACTION action, CM_NOTIFY_EVENT_DATA *eventData, DWORD eventDataSize)
+{
+    std::wstring deviceName;
+
+    switch (action) {
+        case CM_NOTIFY_ACTION_DEVICEINTERFACEARRIVAL: {
+            deviceName = eventData->u.DeviceInterface.SymbolicLink;
+            auto nearObjectDeviceUwb = AddCachedUwbNearObjectDevice(deviceName);
+            DevicePresenceChanged(NearObjectDevicePresence::Arrived, std::move(nearObjectDeviceUwb));
+            break;
+        }
+        case CM_NOTIFY_ACTION_DEVICEINTERFACEREMOVAL: {
+            deviceName = eventData->u.DeviceInterface.SymbolicLink;
+            auto nearObjectDeviceUwb = ExtractCachedNearObjectDevice(deviceName);
+            if (nearObjectDeviceUwb) {
+                DevicePresenceChanged(NearObjectDevicePresence::Departed, std::move(nearObjectDeviceUwb));
+            }
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+/* static */
+DWORD CALLBACK
+NearObjectDeviceDiscoveryAgentUwb::OnDeviceInterfaceNotificationCallback(HCMNOTIFICATION hcmNotificationHandle, void *context, CM_NOTIFY_ACTION action, CM_NOTIFY_EVENT_DATA *eventData, DWORD eventDataSize)
+{
+    auto self = static_cast<NearObjectDeviceDiscoveryAgentUwb *>(context);
+    self->OnDeviceInterfaceNotification(hcmNotificationHandle, action, eventData, eventDataSize);
+    return S_OK;
+}
+
+std::future<std::vector<std::weak_ptr<NearObjectDevice>>>
+NearObjectDeviceDiscoveryAgentUwb::ProbeAsyncImpl()
+{
+    std::promise<std::vector<std::weak_ptr<NearObjectDevice>>> probePromise{};
+    // TODO: save promise somewhere
+    // TODO: start async operation
+    // TODO: call CM_Get_Device_InterfaceList.
+    // TODO: call probePromise.set_value({/* results from CM_Get_Device_InterfaceList */})
+    return probePromise.get_future();
 }
--- windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.hxx
+++ windows/nearobject/service/NearObjectDeviceDiscoveryAgentUwb.hxx
@@ -2,7 +2,16 @@
 #ifndef NEAR_OBJECT_DEVICE_DISCOVERY_AGENT_UWB
 #define NEAR_OBJECT_DEVICE_DISCOVERY_AGENT_UWB
 
+#include <future>
 #include <memory>
+#include <mutex>
+#include <unordered_map>
+#include <vector>
+
+#include <cfgmgr32.h>
+#include <devpkey.h>
+#include <wil/resource.h>
+#include <windows.h>
 
 #include <nearobject/service/NearObjectDeviceDiscoveryAgent.hxx>
 
@@ -10,6 +19,7 @@ namespace nearobject
 {
 namespace service
 {
+class NearObjectDevice;
 class NearObjectDeviceUwb;
 } // namespace service
 } // namespace nearobject
@@ -27,12 +37,67 @@ namespace service
 class NearObjectDeviceDiscoveryAgentUwb :
     public ::nearobject::service::NearObjectDeviceDiscoveryAgent
 {
-private:
-    std::shared_ptr<::nearobject::service::NearObjectDeviceUwb>
-    SignalDeviceAdded(std::unique_ptr<windows::devices::UwbDevice> uwbDevice) const noexcept;
+protected:
+    std::future<std::vector<std::weak_ptr<::nearobject::service::NearObjectDevice>>>
+    ProbeAsyncImpl() override;
 
+private:
+    /**
+     * @brief Register for notifications of UWB device driver events.
+     */
     void
-    SignalDeviceRemoved(std::shared_ptr<::nearobject::service::NearObjectDeviceUwb> nearObjectDeviceUwb) const noexcept;
+    RegisterForUwbDeviceClassNotifications();
+
+    /**
+     * @brief Bound callback function for when a UWB class driver event occurs. 
+     * 
+     * @param hcmNotificationHandle 
+     * @param action 
+     * @param eventData 
+     * @param eventDataSize 
+     */
+    void
+    OnDeviceInterfaceNotification(HCMNOTIFICATION hcmNotificationHandle, CM_NOTIFY_ACTION action, CM_NOTIFY_EVENT_DATA *eventData, DWORD eventDataSize);
+
+    /**
+     * @brief Unbound callback function for when a UWB class driver event occurs. 
+     * 
+     * @param hcmNotificationHandle 
+     * @param context 
+     * @param action 
+     * @param eventData 
+     * @param eventDataSize 
+     * @return DWORD 
+     */
+    static DWORD CALLBACK
+    OnDeviceInterfaceNotificationCallback(HCMNOTIFICATION hcmNotificationHandle, void *context, CM_NOTIFY_ACTION action, CM_NOTIFY_EVENT_DATA *eventData, DWORD eventDataSize);
+
+    /**
+     * @brief Create (if necessary) and add NearObjectDeviceUwb wrapper instance
+     * to the device cache.
+     * 
+     * @param deviceName The device driver name to add/create.
+     * @return std::shared_ptr<::nearobject::service::NearObjectDeviceUwb> 
+     */
+    std::shared_ptr<::nearobject::service::NearObjectDeviceUwb>
+    AddCachedUwbNearObjectDevice(const std::wstring &deviceName);
+
+    /**
+     * @brief Remove and return a NearObjectDeviceUwb device driver instance
+     * from the device cache.
+     * 
+     * @param deviceName The device driver name to remove.
+     * @return std::shared_ptr<::nearobject::service::NearObjectDeviceUwb> 
+     */
+    std::shared_ptr<::nearobject::service::NearObjectDeviceUwb>
+    ExtractCachedNearObjectDevice(const std::wstring &deviceName);
+
+private:
+    using unique_hcmnotification = wil::unique_any<HCMNOTIFICATION, decltype(&::CM_Unregister_Notification), ::CM_Unregister_Notification>;
+    unique_hcmnotification m_uwbHcmNotificationHandle;
+
+    std::mutex m_nearObjectDeviceCacheGate;
+    std::unordered_map<std::wstring, std::weak_ptr<::nearobject::service::NearObjectDeviceUwb>> m_nearObjectDeviceCache;
 };
 } // namespace service
 } // namespace nearobject
