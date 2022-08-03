39eedf65a407ad628e76152948796ef2875cdc6c
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-03 01:27:33 -0600

    Fix GUID constant.

--- windows/devices/uwb/UwbDeviceDriver.hxx
+++ windows/devices/uwb/UwbDeviceDriver.hxx
@@ -2,6 +2,7 @@
 #ifndef UWB_DEVICE_DRIVER_HXX
 #define UWB_DEVICE_DRIVER_HXX
 
+#include <initguid.h>
 #include <guiddef.h>
 
 namespace windows
@@ -16,7 +17,7 @@ namespace uwb
     DEFINE_GUID(GUID_DEVINTERFACE_UWB, 0x1f49c35a, 0xf284, 0x4f81, 0x87, 0xd7, 0x47, 0x25, 0xa6, 0xee, 0xc3, 0xe8);
 #endif
 
-static const GUID InterfaceClassUwb = GUID_DEVINTERFACE_UWB;
+const GUID InterfaceClassUwb = GUID_DEVINTERFACE_UWB;
 }
 }
 } // namespace windows
