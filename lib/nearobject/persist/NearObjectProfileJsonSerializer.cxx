afc459046494badeed48f8590b5f4813365cb69a
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-13 13:32:03 -0600

    Add NearObjectProfile+Security JSON serializations with nlohmann.

--- lib/nearobject/persist/NearObjectProfileJsonSerializer.cxx
+++ lib/nearobject/persist/NearObjectProfileJsonSerializer.cxx
@@ -1,4 +1,5 @@
 
+#include <nlohmann/json.hpp>
 #include <nearobject/persist/NearObjectProfileJsonSerializer.hxx>
 
 using namespace nearobject;
@@ -15,3 +16,32 @@ NearObjectProfilePersister::FromJson(const std::string& json) const
 {
     return {};
 }
+
+using json = nlohmann::json;
+
+namespace nearobject
+{
+void to_json(json& j, const NearObjectConnectionProfileSecurity& security) 
+{
+    j = json{};
+}
+
+void from_json(const json& j, NearObjectConnectionProfileSecurity& security) 
+{
+}
+
+void to_json(json& j, const NearObjectProfile& profile) 
+{
+    j = json {
+        { "Scope", profile.Scope },
+        { "Security", profile.Security.value_or(NearObjectConnectionProfileSecurity{}) },
+    };
+}
+
+void from_json(const json& j, NearObjectProfile& profile) 
+{
+    profile.Security.emplace();
+    j.at("Scope").get_to(profile.Scope);
+    j.at("Security").get_to(profile.Security.value());
+}
+} // namespace nearobject
