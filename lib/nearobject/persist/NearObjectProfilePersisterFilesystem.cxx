33914c0eef7c1dda25feb100c066f29d2afe51d7
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-15 15:33:03 -0600

    Remove unneeded header.

--- lib/nearobject/persist/NearObjectProfilePersisterFilesystem.cxx
+++ lib/nearobject/persist/NearObjectProfilePersisterFilesystem.cxx
@@ -2,7 +2,6 @@
 #include <algorithm>
 #include <fstream>
 
-#include <JsonSerializer.hxx>
 #include <nearobject/persist/NearObjectProfileJsonSerializer.hxx>
 #include <nearobject/persist/NearObjectProfilePersisterFilesystem.hxx>
 
