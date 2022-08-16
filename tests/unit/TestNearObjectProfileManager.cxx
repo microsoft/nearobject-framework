0b61c29197a38a8b252ee0dc18fda9a247034911
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-16 16:41:54 -0600

    Remove unused code.

--- tests/unit/TestNearObjectProfileManager.cxx
+++ tests/unit/TestNearObjectProfileManager.cxx
@@ -8,32 +8,6 @@
 #include <nearobject/service/NearObjectProfileManager.hxx>
 #include <nearobject/persist/NearObjectProfilePersisterFilesystem.hxx>
 
-namespace nearobject
-{
-namespace service
-{
-namespace test
-{
-class TestNearObjectProfileManager 
-    : public NearObjectProfileManager
-{
-public:
-    persist::PersistResult
-    TestPersistProfile(const NearObjectProfile& profile)
-    {
-        return NearObjectProfileManager::PersistProfile(profile);
-    }
-
-    std::vector<NearObjectProfile>
-    TestReadPersistedProfiles(persist::PersistResult& persistResult) const
-    {
-        return NearObjectProfileManager::ReadPersistedProfiles(persistResult);
-    }
-};
-} // namespace test
-} // namespace service
-} // namespace nearobject
-
 TEST_CASE("near object profile manager can be created", "[basic][service]")
 {
     using namespace nearobject::persistence;
@@ -80,52 +54,6 @@ TEST_CASE("near object profiles can be persisted", "[basic][infra]")
         throw std::filesystem::filesystem_error("could not create test directory", std::error_code());
     }
 
-    SECTION("NearObjectProfileManager::PersistProfile matches the read profiles")
-    {
-        using namespace nearobject;
-        using namespace nearobject::service;
-
-        NearObjectProfileSecurity security;
-        NearObjectProfile profile1{ NearObjectConnectionScope::Unicast, std::move(security) };
-        NearObjectProfile profile2;
-
-        // rapidjson::Document document;
-        // auto& allocator = document.GetAllocator();
-
-        // const auto persistLocation = testTempDirectory / std::filesystem::path("profiles");
-
-        // // remove the file
-        // try {
-        //     std::filesystem::remove(persistLocation);
-        // } catch (const std::filesystem::filesystem_error&) {
-        // }
-
-        // test::TestNearObjectProfileManager profileManager{};
-
-        // // persist the profiles
-        // auto result = profileManager.TestPersistProfile(profile1);
-        // REQUIRE(result == persist::PersistResult::Succeeded);
-        // result = profileManager.TestPersistProfile(profile2);
-        // REQUIRE(result == persist::PersistResult::Succeeded);
-
-        // // read the profiles
-        // persist::PersistResult persistResult = persist::PersistResult::UnknownError;
-        // auto profiles = profileManager.TestReadPersistedProfiles(persistResult);
-
-        // // remove the file
-        // try {
-        //     std::filesystem::remove(persistLocation);
-        // } catch (const std::filesystem::filesystem_error&) {
-        // }
-
-        // REQUIRE(persistResult == persist::PersistResult::Succeeded);
-        // REQUIRE(profiles.size() == 2);
-        // REQUIRE((profile1 == profiles[0] || profile1 == profiles[1]));
-        // if (profile1 == profiles[0]) {
-        //     REQUIRE(profile2 == profiles[1]);
-        // } else {
-        //     REQUIRE(profile2 == profiles[0]);
-        // }
-    }
-    std::filesystem::remove(testTempDirectory);
+TEST_CASE("near object profiles can be persisted", "[basic][infra]")
+{
 }
