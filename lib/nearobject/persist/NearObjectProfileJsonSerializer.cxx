20e412569430889a5a56f4e0c03e5948b38f5d93
Andrew Beltrano <anbeltra@microsoft.com>
2022-08-15 21:10:28 -0600

    Serialize optional Security correctly.

--- lib/nearobject/persist/NearObjectProfileJsonSerializer.cxx
+++ lib/nearobject/persist/NearObjectProfileJsonSerializer.cxx
@@ -1,17 +1,18 @@
 
 #include <nearobject/persist/NearObjectProfileJsonSerializer.hxx>
 #include <nearobject/NearObjectProfile.hxx>
+#include <nlohmann/std_optional_serializer.hxx>
 
 using namespace nearobject;
 
 void
-nearobject::to_json(nlohmann::json& json, const NearObjectConnectionProfileSecurity& security) 
+nearobject::to_json(nlohmann::json& json, const NearObjectProfileSecurity& security) 
 {
     json = nlohmann::json{};
 }
 
 void
-nearobject::from_json(const nlohmann::json& json, NearObjectConnectionProfileSecurity& security) 
+nearobject::from_json(const nlohmann::json& json, NearObjectProfileSecurity& security) 
 {
 }
 
@@ -20,14 +21,13 @@ nearobject::to_json(nlohmann::json& json, const NearObjectProfile& profile)
 {
     json = nlohmann::json {
         { "Scope", profile.Scope },
-        { "Security", profile.Security.value_or(NearObjectConnectionProfileSecurity{}) },
+        { "Security", profile.Security },
     };
 }
 
 void
 nearobject::from_json(const nlohmann::json& json, NearObjectProfile& profile) 
 {
-    profile.Security.emplace();
     json.at("Scope").get_to(profile.Scope);
-    json.at("Security").get_to(profile.Security.value());
+    json.at("Security").get_to(profile.Security);
 }
