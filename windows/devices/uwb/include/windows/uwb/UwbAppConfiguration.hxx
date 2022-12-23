df2579ed62773f94e6f141cd6eee91df7605c604
Andrew Beltrano <abeltrano@gmail.com>
2022-12-23 08:18:22 -0700

    Formatting.

--- windows/devices/uwb/include/windows/uwb/UwbAppConfiguration.hxx
+++ windows/devices/uwb/include/windows/uwb/UwbAppConfiguration.hxx
@@ -15,7 +15,7 @@ template <typename PropertyT>
 class UwbAppConfigurationParameter
 {
 public:
-    UwbAppConfigurationParameter(const PropertyT& value, size_t parameterSize = sizeof(PropertyT)) : 
+    UwbAppConfigurationParameter(const PropertyT& value, size_t parameterSize = sizeof(PropertyT)) :
         m_size(offsetof(UWB_APP_CONFIG_PARAM, paramValue[sizeof(PropertyT)])),
         m_buffer(std::make_unique<uint8_t[]>(m_size)),
         m_parameter(*reinterpret_cast<UWB_APP_CONFIG_PARAM*>(m_buffer.get())),
@@ -24,43 +24,43 @@ public:
         m_parameter.size = m_size;
         m_parameter.paramLength = parameterSize;
         m_value = value;
-    } 
+    }
 
     std::size_t
     Size() noexcept
     {
-        return m_size; 
-    } 
+        return m_size;
+    }
 
     uint8_t*
-    Buffer() noexcept 
+    Buffer() noexcept
     {
         return m_buffer;
-    } 
-   
-    PropertyT& 
+    }
+
+    PropertyT&
     Value() noexcept
     {
-        return m_value; 
-    } 
+        return m_value;
+    }
 
-    UWB_APP_CONFIG_PARAM& 
+    UWB_APP_CONFIG_PARAM&
     DdiParameter() noexcept
     {
-        return m_parameter; 
-    } 
+        return m_parameter;
+    }
 
     std::tuple<uint8_t*, std::size_t>
     GetIoctlBuffer() noexcept
     {
-        return { m_buffer, m_size }; 
+        return { m_buffer, m_size };
     }
 
- private: 
-    // order of members here is important to enforce proper initialization order 
+private:
+    // order of members here is important to enforce proper initialization order
     std::size_t m_size;
-    std::unique_ptr<uint8_t[]> m_buffer; 
-    UWB_APP_CONFIG_PARAM &m_parameter; 
+    std::unique_ptr<uint8_t[]> m_buffer;
+    UWB_APP_CONFIG_PARAM& m_parameter;
     PropertyT& m_value;
 };
 } // namespace windows::devices
