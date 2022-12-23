e7ac617ef34bffca922711f24c7b3371d8214f3d
Andrew Beltrano <abeltrano@gmail.com>
2022-12-23 08:12:47 -0700

    Fix size bug.

--- windows/devices/uwb/include/windows/uwb/UwbAppConfiguration.hxx
+++ windows/devices/uwb/include/windows/uwb/UwbAppConfiguration.hxx
@@ -11,37 +11,57 @@
 
 namespace windows::devices
 {
-template <typename T>
+template <typename PropertyT>
 class UwbAppConfigurationParameter
 {
-    UwbAppConfigurationParameter(const T& value) : 
-        m_size(offsetof(UWB_APP_CONFIG_PARAM), paramValue[sizeof(T)]),
-        m_buffer(std::make_unique<uint8_t[]>(m_parameter.size),
+public:
+    UwbAppConfigurationParameter(const PropertyT& value, size_t parameterSize = sizeof(PropertyT)) : 
+        m_size(offsetof(UWB_APP_CONFIG_PARAM, paramValue[sizeof(PropertyT)])),
+        m_buffer(std::make_unique<uint8_t[]>(m_size)),
         m_parameter(*reinterpret_cast<UWB_APP_CONFIG_PARAM*>(m_buffer.get())),
-        m_parameter.size(m_size),
-        m_parameter.paramLength = sizeof(T),
-        m_value(reinterpret_cast<T&>(m_parameter.paramValue))
+        m_value(reinterpret_cast<PropertyT&>(m_parameter.paramValue))
     {
+        m_parameter.size = m_size;
+        m_parameter.paramLength = sizeof(PropertyT);
         m_value = value; // set value contents 
     } 
 
-    auto Size() {
-        return m_size; } 
-    uint8_t* Buffer() {
-        return m_buffer; } 
-    T& Value() {
-        return m_value; } 
-    UWB_APP_CONFIG_PARAM& Parameter() {
-        m_parameter; } 
-    std::tuple<uint8_t*, std::size_t> GetIoctlBuffer() {
-        return { m_buffer, m_size }; }
+    std::size_t
+    Size() noexcept
+    {
+        return m_size; 
+    } 
+
+    uint8_t*
+    Buffer() noexcept 
+    {
+        return m_buffer;
+    } 
+   
+    PropertyT& 
+    Value() noexcept
+    {
+        return m_value; 
+    } 
+
+    UWB_APP_CONFIG_PARAM& 
+    DdiParameter() noexcept
+    {
+        return m_parameter; 
+    } 
+
+    std::tuple<uint8_t*, std::size_t>
+    GetIoctlBuffer() noexcept
+    {
+        return { m_buffer, m_size }; 
+    }
 
  private: 
-    // order of members here is intentional, to enforce proper initialization order 
+    // order of members here is important to enforce proper initialization order 
     std::size_t m_size;
     std::unique_ptr<uint8_t[]> m_buffer; 
     UWB_APP_CONFIG_PARAM &m_parameter; 
-    T& m_value;
+    PropertyT& m_value;
 };
 } // namespace windows::devices
 
