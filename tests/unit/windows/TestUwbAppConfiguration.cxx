bf8b896910d29187c3633960fddb2e2c20b851b3
Andrew Beltrano <abeltrano@gmail.com>
2022-12-23 08:12:56 -0700

    Add basic test.

--- tests/unit/windows/TestUwbAppConfiguration.cxx
+++ tests/unit/windows/TestUwbAppConfiguration.cxx
@@ -2,15 +2,24 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <uwb/protocols/fira/FiraDevice.hxx>
-#include <uwb/protocols/fira/UwbAppConfiguration.hxx>
+#include <windows/uwb/UwbAppConfiguration.hxx>
 
 TEST_CASE("UwbAppConfiguration performs allocation for contained value correctly", "[basic]")
 {
-    using namespace uwb::protocols::fira;
+    using windows::devices::UwbAppConfigurationParameter;
+    using namespace uwb::protocol::fira;
 
-    SECTION("random")
+    SECTION("parameter with POD-type works")
     {
-        DeviceRole role{ DeviceRole::Initiator };  
+        constexpr DeviceRole roleExpected{ DeviceRole::Initiator };
+        UwbAppConfigurationParameter<DeviceRole> appConfiguration{ roleExpected };
+        auto& roleActual = appConfiguration.Value();
+        REQUIRE(roleActual == roleExpected);
+
+        std::size_t sizeExpected = sizeof roleExpected + 0;
+        auto sizeActual = appConfiguration.Size();
+        // REQUIRE(sizeActual == sizeExpected);
+
+        auto appConfigurationDdi = appConfiguration.DdiParameter();
     }
 }
-
