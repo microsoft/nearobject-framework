8b65cc510c5780ba35dd3e4352fbd5db0bdb44ae
Shane Guan <shaneguan@microsoft.com>
2023-01-02 14:21:20 -0800

    builder

---
+++ windows/devices/uwb/UwbAppConfiguration.cxx
@@ -0,0 +1,32 @@
+
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <tuple>
+
+#include <windows/devices/uwb/UwbAppConfiguration.hxx>
+
+windows::devices::UwbSetAppConfigurationParametersBuilder::UwbSetAppConfigurationParametersBuilder(uint32_t sessionId) :
+    m_sessionId{ sessionId }
+{}
+
+std::unique_ptr<uint8_t[]>
+windows::devices::UwbSetAppConfigurationParametersBuilder::Publish(){
+    auto desiredSize = offsetof(UWB_SET_APP_CONFIG_PARAMS, appConfigParams[0]) + m_paramsLengthSum;
+    auto buffer = std::make_unique<uint8_t[]>(desiredSize);
+    UWB_SET_APP_CONFIG_PARAMS& setParams = *reinterpret_cast<UWB_SET_APP_CONFIG_PARAMS*>(buffer.get());
+
+    setParams.size = desiredSize;
+    setParams.sessionId = m_sessionId;
+    setParams.appConfigParamsCount = m_params.size();
+
+    uint8_t *dstBuffer = reinterpret_cast<uint8_t*>(setParams.appConfigParams);
+
+    for(const auto& paramBuffer : m_params){
+        auto& param = *reinterpret_cast<UWB_APP_CONFIG_PARAM*>(paramBuffer.get());
+        memcpy(dstBuffer,paramBuffer.get(),param.size);
+        dstBuffer = &dstBuffer[param.size];
+    }
+    return buffer;
+}
--- windows/devices/uwb/include/windows/devices/uwb/UwbAppConfiguration.hxx
+++ windows/devices/uwb/include/windows/devices/uwb/UwbAppConfiguration.hxx
@@ -7,20 +7,23 @@
 #include <cstring>
 #include <memory>
 #include <tuple>
+#include <vector>
 
 #include <UwbCxLrpDeviceGlue.h>
 
 namespace windows::devices
 {
+class UwbSetAppConfigurationParametersBuilder;
 /**
- * @brief Adaptor for using UWB_APP_CONFIG_PARAM UwbCx DDI structure in a nicer way. 
- * 
+ * @brief Adaptor for using UWB_APP_CONFIG_PARAM UwbCx DDI structure in a nicer way.
+ *
  * @tparam PropertyT The type of property the UWB_APP_CONFIG_PARAM structure
  * holds as its flexible array member.
  */
 template <typename PropertyT>
 class UwbAppConfigurationParameter
 {
+    friend class UwbSetAppConfigurationParametersBuilder; // friend class so the unique buffer ptr can be moved out
 public:
     explicit UwbAppConfigurationParameter(const PropertyT& value, UWB_APP_CONFIG_PARAM_TYPE parameterType, size_t parameterSize = sizeof(PropertyT)) :
         m_size(offsetof(UWB_APP_CONFIG_PARAM, paramValue[parameterSize])),
@@ -35,9 +38,9 @@ public:
     }
 
     /**
-     * @brief The total size of the UWB_APP_CONFIG_PARAM structure. 
-     * 
-     * @return std::size_t 
+     * @brief The total size of the UWB_APP_CONFIG_PARAM structure.
+     *
+     * @return std::size_t
      */
     std::size_t
     Size() noexcept
@@ -48,8 +51,8 @@ public:
     /**
      * @brief The buffer containing the complete UWB_APP_CONFIG_PARAM structure,
      * along with the trailing parameter value.
-     * 
-     * @return uint8_t* 
+     *
+     * @return uint8_t*
      */
     uint8_t*
     Buffer() noexcept
@@ -59,8 +62,8 @@ public:
 
     /**
      * @brief Return a typed reference to the flexible array member value.
-     * 
-     * @return PropertyT& 
+     *
+     * @return PropertyT&
      */
     PropertyT&
     Value() noexcept
@@ -69,9 +72,9 @@ public:
     }
 
     /**
-     * @brief Reference to the UwbCx DDI structure. 
-     * 
-     * @return UWB_APP_CONFIG_PARAM& 
+     * @brief Reference to the UwbCx DDI structure.
+     *
+     * @return UWB_APP_CONFIG_PARAM&
      */
     UWB_APP_CONFIG_PARAM&
     DdiParameter() noexcept
@@ -80,9 +83,9 @@ public:
     }
 
     /**
-     * @brief The buffer and associated size that is suitable for passing to the UwbCx DDI. 
-     * 
-     * @return std::tuple<uint8_t*, std::size_t> 
+     * @brief The buffer and associated size that is suitable for passing to the UwbCx DDI.
+     *
+     * @return std::tuple<uint8_t*, std::size_t>
      */
     std::tuple<uint8_t*, std::size_t>
     DdiBuffer() noexcept
@@ -96,6 +99,36 @@ private:
     std::unique_ptr<uint8_t[]> m_buffer;
     UWB_APP_CONFIG_PARAM& m_parameter;
     PropertyT& m_value;
+};
+
+/**
+ * Builder class that takes in a collection of UwbAppConfigurationParameter and outputs a UWB_SET_APP_CONFIG_PARAMS
+ */
+class UwbSetAppConfigurationParametersBuilder
+{
+public:
+    UwbSetAppConfigurationParametersBuilder(uint32_t sessionId);
+
+    /**
+     * Push a UwbAppConfigurationParameter into the collection
+    */
+    template <typename PropertyT>
+    void
+    Push(UwbAppConfigurationParameter<PropertyT> param){
+       m_params.push_back(std::move(param.m_buffer));
+       m_paramsLengthSum += param.Size();
+    }
+
+    /**
+     * Writes all the params and returns a buffer that can be reinterpreted as a UWB_SET_APP_CONFIG_PARAMS
+    */
+    std::unique_ptr<uint8_t[]>
+    Publish();
+
+private:
+    uint32_t m_sessionId;
+    std::size_t m_paramsLengthSum;
+    std::vector<std::unique_ptr<uint8_t[]>> m_params;
 };
 } // namespace windows::devices
 
