1017a9a9aaf2e89bb69dd62ea3a277b454a39a1c
Andrew Beltrano <abeltrano@gmail.com>
2023-01-10 10:59:51 -0700

    Add file create+close trace events.

--- UwbSimulatorDevice.cxx
+++ UwbSimulatorDevice.cxx
@@ -1,13 +1,27 @@
 
 #include "UwbSimulatorDevice.hxx"
+#include "UwbSimulatorTracelogging.hxx"
 
 /* static */
 void
-UwbSimulatorDevice::OnFileCreate(WDFDEVICE /* device */, WDFREQUEST /* request */, WDFFILEOBJECT /* file */)
+UwbSimulatorDevice::OnFileCreate(WDFDEVICE device, WDFREQUEST request, WDFFILEOBJECT file)
 {
+    TraceLoggingWrite(
+        UwbSimulatorTraceloggingProvider, 
+        "OnFileCreate", 
+        TraceLoggingLevel(TRACE_LEVEL_INFORMATION),
+        TraceLoggingPointer(device, "Device"),
+        TraceLoggingPointer(request, "Request"),
+        TraceLoggingPointer(file, "File")
+    );
 }
 
 void
-UwbSimulatorDevice::OnFileClose(WDFFILEOBJECT /* file */)
+UwbSimulatorDevice::OnFileClose(WDFFILEOBJECT file)
 {
+    TraceLoggingWrite(
+        UwbSimulatorTraceloggingProvider,
+        "OnFileClose",
+        TraceLoggingLevel(TRACE_LEVEL_INFORMATION),
+        TraceLoggingPointer(file, "File"));
 }
