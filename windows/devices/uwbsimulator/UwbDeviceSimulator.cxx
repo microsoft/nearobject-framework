02184882a197a7415260fda9da3347184510c733
Andrew Beltrano <abeltrano@gmail.com>
2023-01-12 20:51:00 -0700

    Add impl to open handle to driver.

--- windows/devices/uwbsimulator/UwbDeviceSimulator.cxx
+++ windows/devices/uwbsimulator/UwbDeviceSimulator.cxx
@@ -1,2 +1,31 @@
 
 #include <windows/devices/uwb/simulator/UwbDeviceSimulator.hxx>
+
+#include <UwbSimulatorDdiGlue.h>
+
+using namespace windows::devices::uwb::simulator;
+
+UwbDeviceSimulator::UwbDeviceSimulator(std::string deviceName) :
+    m_deviceName(std::move(deviceName))
+{}
+
+const std::string&
+UwbDeviceSimulator::DeviceName() const noexcept
+{
+    return m_deviceName;
+}
+
+void
+UwbDeviceSimulator::Initialize()
+{
+    wil::unique_hfile handleDriver(CreateFileA(
+        m_deviceName.c_str(),
+        GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        nullptr,
+        OPEN_EXISTING,
+        FILE_FLAG_OVERLAPPED,
+        nullptr));
+
+    m_handleDriver = std::move(handleDriver);
+}
