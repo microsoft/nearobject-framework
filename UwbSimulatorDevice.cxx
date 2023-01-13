4453d235d794659500b45a970ad216ba56c731bb
Andrew Beltrano <abeltrano@gmail.com>
2023-01-13 09:50:46 -0700

    Add missing comment.

--- UwbSimulatorDevice.cxx
+++ UwbSimulatorDevice.cxx
@@ -7,17 +7,17 @@ void
 UwbSimulatorDevice::OnFileCreate(WDFDEVICE device, WDFREQUEST request, WDFFILEOBJECT file)
 {
     TraceLoggingWrite(
-        UwbSimulatorTraceloggingProvider, 
-        "OnFileCreate", 
+        UwbSimulatorTraceloggingProvider,
+        "OnFileCreate",
         TraceLoggingLevel(TRACE_LEVEL_INFORMATION),
         TraceLoggingPointer(device, "Device"),
         TraceLoggingPointer(request, "Request"),
-        TraceLoggingPointer(file, "File")
-    );
+        TraceLoggingPointer(file, "File"));
 
     WdfRequestComplete(request, STATUS_SUCCESS);
 }
 
+/* static */
 void
 UwbSimulatorDevice::OnFileClose(WDFFILEOBJECT file)
 {
