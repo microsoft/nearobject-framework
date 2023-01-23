9b24c9177cef3cdaee34c0c137e50a5c09ba2c94
Andrew Beltrano <abeltrano@gmail.com>
2023-01-23 14:03:36 -0700

    Rev portfile release to v0.2.0.

--- ports/nearobject-framework/portfile.cmake
+++ ports/nearobject-framework/portfile.cmake
@@ -1,10 +1,11 @@
 
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL "https://github.com/aep-microsoft/nearobject-framework.git"
-    REF 6235d1afc5e31b0a63c5591d5548f7cc17850971
+    REPO microsoft/nearobject-framework
+    REF v0.2.0
+    SHA512 8e18b09c9f8b0594d299d70bfbbe29c44de5959dd4b10bbc07dccbcfbe6a1629d5db1d18200442735da4d4a399b4ffa4ee13e4bf96ecf5117b485a3f679f502e
+    HEAD_REF develop
 )
-
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
@@ -12,9 +13,7 @@ vcpkg_cmake_configure(
         -DNOF_USE_VCPKG=TRUE
         -DNOF_OFFICIAL_BUILD=TRUE
 )
-
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
-
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
