a7c12ad21b01209b0cbe0abf1f063d4eb4a9253e
Andrew Beltrano <abeltrano@gmail.com>
2023-01-24 14:04:25 -0700

    Restore official build flag.

--- ports/nearobject-framework-umdf/portfile.cmake
+++ ports/nearobject-framework-umdf/portfile.cmake
@@ -2,13 +2,14 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/microsoft/nearobject-framework.git
-    REF c7dddb18316c37fc89659e63bc18271ac2609250
+    REF c084972e4643a075feaf7c93f75295e2f1b13fea
 )
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DNOF_DISABLE_TESTS=TRUE
         -DNOF_USE_VCPKG=TRUE
+        -DNOF_OFFICIAL_BUILD=TRUE
         -DNOF_BUILD_FOR_UMDF=TRUE
 )
 vcpkg_cmake_install()
