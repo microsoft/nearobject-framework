197bccfbcaf431422d98d9c7077b7230dd4e4e49
Andrew Beltrano <abeltrano@gmail.com>
2023-01-31 13:06:35 -0700

    Move DDI adaption functions to uwbcxadaption lib.

---
+++ windows/devices/uwb/include/windows/devices/uwb/UwbCxAdapterDdiLrp.hxx
@@ -0,0 +1,60 @@
+
+#ifndef UWB_CX_ADAPTER_DDI_LRP_HXX
+#define UWB_CX_ADAPTER_DDI_LRP_HXX
+
+#include <uwb/protocols/fira/FiraDevice.hxx>
+
+#include <UwbCxLrpDeviceGlue.h>
+
+/**
+ * @brief Define LRP DDI conversion functionality as free functions in a
+ * namespace.
+ * 
+ * The namespace name is intended to be used explicitly in the
+ * function call, which can help identify the type of conversion being done.
+ * This allows usage such as:
+ * 
+ *  UwbStatus uwbStatus = GetStatus();
+ *  UWB_STATUS ddiStatus = uwb::ddi::lrp::From(uwbStatus);
+ */
+namespace windows::devices::uwb::ddi::lrp
+{
+/**
+ * @brief Converts UwbStatus to UWB_STATUS.
+ * 
+ * @param uwbStatus 
+ * @return UWB_STATUS 
+ */
+UWB_STATUS
+From(const ::uwb::protocol::fira::UwbStatus &uwbStatus);
+
+/**
+ * @brief Converts UwbSessionState to UWB_SESSION_STATE.
+ * 
+ * @param uwbSessionState 
+ * @return UWB_SESSION_STATE 
+ */
+UWB_SESSION_STATE
+From(const ::uwb::protocol::fira::UwbSessionState uwbSessionState);
+
+/**
+ * @brief Converts UwbDeviceInfoInformation to UWB_DEVICE_INFO.
+ * 
+ * @param uwbDeviceInfo 
+ * @return UWB_DEVICE_INFO 
+ */
+UWB_DEVICE_INFO
+From(const ::uwb::protocol::fira::UwbDeviceInfoInformation &uwbDeviceInfo);
+
+/**
+ * @brief Converts UwbDeviceCapabilities to UWB_DEVICE_CAPABILITIES.
+ * 
+ * @param uwbDeviceCapabilities 
+ * @return UWB_DEVICE_CAPABILITIES 
+ */
+UWB_DEVICE_CAPABILITIES
+From(const ::uwb::protocol::fira::UwbDeviceCapabilities &uwbDeviceCapabilities);
+
+} // namespace windows::devices::uwb::ddi::lrp
+
+#endif // UWB_CX_ADAPTER_DDI_LRP_HXX
