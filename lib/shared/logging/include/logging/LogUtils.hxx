fd2160ad54d250db36e528a64f6f29f07aa2e8c0
Shane Guan <shaneguan@microsoft.com>
2023-02-01 12:02:13 -0800

    fix docs

--- lib/shared/logging/include/logging/LogUtils.hxx
+++ lib/shared/logging/include/logging/LogUtils.hxx
@@ -4,10 +4,20 @@
 
 #include <string>
 
-namespace logging 
+namespace logging
 {
 
-std::string GetLogName(const std::string& executableName);
+/**
+ * @brief Returns the name of the log file to be used
+ * Format is <date>-LogNearObject-<executableName>.txt
+ * For example, if the date was Jan 2, 2001 and the executableName was nocli, then the name would be
+ * 20010102-LogNearObject-nocli.txt
+ *
+ * @param executableName
+ * @return std::string
+ */
+std::string
+GetLogName(const std::string& executableName);
 
 } // namespace logging
 
