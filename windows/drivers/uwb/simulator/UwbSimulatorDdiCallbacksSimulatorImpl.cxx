5c966ab28600221af40f34727f6b232a3c230a24
Andrew Beltrano <abeltrano@gmail.com>
2023-02-28 20:18:17 -0700

    Add switch for trigger event.

--- windows/drivers/uwb/simulator/UwbSimulatorDdiCallbacksSimulatorImpl.cxx
+++ windows/drivers/uwb/simulator/UwbSimulatorDdiCallbacksSimulatorImpl.cxx
@@ -14,6 +14,20 @@ UwbSimulatorDdiCallbacksSimulatorImpl::GetSimulatorCapabilities()
 }
 
 void
-UwbSimulatorDdiCallbacksSimulatorImpl::TriggerSessionEvent(const UwbSimulatorTriggerSessionEventArgs & /*triggerSessionEventArgs*/)
+UwbSimulatorDdiCallbacksSimulatorImpl::TriggerSessionEvent(const UwbSimulatorTriggerSessionEventArgs &triggerSessionEventArgs)
 {
+	switch (triggerSessionEventArgs.Action) {
+    case UwbSimulatorSessionEventAction::RandomRangingMeasurementGenerationStart: {
+        break;
+	}
+    case UwbSimulatorSessionEventAction::RandomRangingMeasurementGenerationStop: {
+        break;
+    }
+    case UwbSimulatorSessionEventAction::None: {
+        break;
+    }
+    default: {
+        break;
+    }
+	}
 }
