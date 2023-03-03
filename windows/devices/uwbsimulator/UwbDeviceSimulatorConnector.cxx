5f7c267da65000b21705dab140a3b221580f7127
Andrew Beltrano <abeltrano@gmail.com>
2023-03-03 11:08:47 -0700

    Implement uwb simulator connector GetCapabilites().

--- windows/devices/uwbsimulator/UwbDeviceSimulatorConnector.cxx
+++ windows/devices/uwbsimulator/UwbDeviceSimulatorConnector.cxx
@@ -1,6 +1,16 @@
 
+#include <ios>
+#include <iomanip>
+
+#include <plog/Log.h>
+#include <wil/result.h>
+
+#include <ioapiset.h>
+
+#include <windows/devices/DeviceHandle.hxx>
 #include <windows/devices/uwb/simulator/UwbDeviceSimulatorConnector.hxx>
 
+using namespace windows::devices;
 using namespace windows::devices::uwb;
 using namespace windows::devices::uwb::simulator;
 
@@ -13,7 +23,23 @@ UwbDeviceSimulatorConnector::GetCapabilites()
 {
     std::promise<UwbSimulatorCapabilities> resultPromise;
     auto resultFuture = resultPromise.get_future();
-    // TODO: invoke IOCTL_UWB_DEVICE_SIM_GET_CAPABILITIES
+
+    wil::unique_hfile handleDriver;
+    auto hr = OpenDriverHandle(handleDriver, m_deviceName.c_str());
+    if (FAILED(hr)) {
+        PLOG_ERROR << "failed to obtain driver handle for " << m_deviceName << ", hr=" << std::showbase << std::hex << hr;
+        return resultFuture;
+    }
+
+    UwbSimulatorCapabilities simulatorCapabilities{};
+    DWORD bytesRequired = sizeof simulatorCapabilities;
+    BOOL ioResult = DeviceIoControl(handleDriver.get(), IOCTL_UWB_DEVICE_SIM_GET_CAPABILITIES, nullptr, 0, &simulatorCapabilities, sizeof simulatorCapabilities, nullptr, nullptr);
+    if (!ioResult) {
+        HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
+        PLOG_ERROR << "error when sending IOCTL_UWB_DEVICE_SIM_GET_CAPABILITIES, hr=" << std::showbase << std::hex << hr;
+    }
+
+    resultPromise.set_value(std::move(simulatorCapabilities));
 
     return resultFuture;
 }
