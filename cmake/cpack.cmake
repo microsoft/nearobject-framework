15651c533026a6a2219dd0f9e0af34f17ef4e890
Andrew Beltrano <abeltrano@gmail.com>
2023-03-10 21:21:36 -0700

    Add cpack cmake script.

---
+++ cmake/cpack.cmake
@@ -0,0 +1,14 @@
+
+# Metadata related varaibles.
+set(CPACK_PACKAGE_NAME "nearobjectframework")
+set(CPACK_PACKAGE_VENDOR "Microsoft")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Near Object Framework - A framework for interacting with short-range devices")
+
+# Package configuration.
+set(CPACK_NSIS OFF)
+set(CPACK_BINARY_NSIS OFF)
+set(CPACK_ZIP ON)
+set(CPACK_BINARY_ZIP ON)
+set(CPACK_SOURCE_ZIP ON)  
+
+include(CPack)
