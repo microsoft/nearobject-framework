aac45c3a4ab287fe57ed93af4ce829e1715b117c
Andrew Beltrano <abeltrano@gmail.com>
2023-03-13 10:59:41 -0600

    Fix generator spec.

--- cmake/cpack.cmake
+++ cmake/cpack.cmake
@@ -4,18 +4,6 @@ set(CPACK_PACKAGE_NAME ${CMAKE_PROJECT_NAME})
 set(CPACK_PACKAGE_VENDOR "Microsoft")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Near Object Framework - A framework for interacting with short-range devices")
 
-# Append the build type to the package file name. Solution taken from Craig
-# Scott in https://gitlab.kitware.com/cmake/cmake/-/issues/10940.
-if(CMAKE_GENERATOR STREQUAL Xcode OR
-   CMAKE_GENERATOR MATCHES "^Visual Studio.*")
-    # Multi-config generator, so we don't know the build config right now.
-    # Delay evaluation of the build config until CPack runs.
-    # set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}-\${CPACK_BUILD_CONFIG}")
-else()
-    # We know the build config now, so use it directly
-    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}-${CMAKE_SYSTEM_NAME}-${CMAKE_SYSTEM_PROCESSOR}-${CMAKE_BUILD_TYPE}")
-endif()
-
 # Package configuration.
 set(CPACK_NSIS OFF)
 set(CPACK_BINARY_NSIS OFF)
