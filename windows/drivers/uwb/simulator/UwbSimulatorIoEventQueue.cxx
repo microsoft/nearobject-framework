3ac45cb9f142e55591ac98e1da6d3fe33182ddc0
Andrew Beltrano <abeltrano@gmail.com>
2023-03-27 16:58:58 -0600

    Update output buffer size.

--- windows/drivers/uwb/simulator/UwbSimulatorIoEventQueue.cxx
+++ windows/drivers/uwb/simulator/UwbSimulatorIoEventQueue.cxx
@@ -51,6 +51,7 @@ UwbSimulatorIoEventQueue::HandleNotificationRequest(WDFREQUEST request, std::opt
         auto converted = UwbCxDdi::From(notificationData);
         auto outputBufferSizeRequired = converted.size();
         if (outputBufferSize < outputBufferSizeRequired) {
+            outputBufferSize = outputBufferSizeRequired;
             status = STATUS_BUFFER_OVERFLOW;
         } else {
             notificationDataOpt = std::move(notificationData);
