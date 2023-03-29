f060ef2d0411ff6a353613d15248aef9bb8ad7e4
Andrew Beltrano <abeltrano@gmail.com>
2023-03-29 07:20:20 -0600

    Print session id in dec, right-align, and width.

--- tools/cli/NearObjectCliUwbSessionEventCallbacks.cxx
+++ tools/cli/NearObjectCliUwbSessionEventCallbacks.cxx
@@ -25,7 +25,7 @@ public:
     std::ostream&
     operator()(std::ostream& out) const
     {
-        return out << "[Session " << std::showbase << std::hex << std::setw(sizeof(m_sessionId)) << std::setfill('0') << std::internal << m_sessionId << "]: ";
+        return out << "[Session " << std::setw(10) << std::setfill(' ') << std::right << m_sessionId << "]: ";
     }
 
 private:
