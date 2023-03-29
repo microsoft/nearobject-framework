b42d8bad4304625442c5db0be07fd0bb2cdbb95d
Andrew Beltrano <abeltrano@gmail.com>
2023-03-29 06:30:58 -0600

    Add consistent log prefix.

--- tools/cli/NearObjectCliUwbSessionEventCallbacks.cxx
+++ tools/cli/NearObjectCliUwbSessionEventCallbacks.cxx
@@ -11,11 +11,47 @@ NearObjectCliUwbSessionEventCallbacks::NearObjectCliUwbSessionEventCallbacks(std
     m_onSessionEndedCallback(std::move(onSessionEndedCallback))
 {}
 
+namespace
+{
+/**
+ * @brief Helper stream manipulator to add a common log prefix.
+ */
+class LogPrefix
+{
+public:
+    LogPrefix(uint32_t sessionId) :
+        m_sessionId(sessionId) {}
+
+    std::ostream&
+    operator()(std::ostream& out) const
+    {
+        return out << "[Session " << std::showbase << std::hex << std::setw(sizeof(m_sessionId)) << std::internal << m_sessionId << "]: ";
+    }
+
+private:
+    uint32_t m_sessionId;
+};
+
+/**
+ * @brief Stream specialization taking a LogPrefix argument, allowing
+ * std::ostream << LogPrefix(value).
+ *
+ * @param out The stream to manipulate.
+ * @param logPrefix The instance to manipulate the stream.
+ * @return std::ostream&
+ */
+std::ostream&
+operator<<(std::ostream& out, LogPrefix logPrefix)
+{
+    return logPrefix(out);
+}
+} // namespace
+
 void
 NearObjectCliUwbSessionEventCallbacks::OnSessionEnded(::uwb::UwbSession* session, ::uwb::UwbSessionEndReason /* reason */)
 {
-    std::cout << "Session with id="
-              << std::hex << std::setw(8) << std::setfill('0') << std::showbase << std::internal << session->GetId() << ": Session Ended" << std::endl;
+    std::cout << LogPrefix(session->GetId()) << "Session Ended" << std::endl;
+
     if (m_onSessionEndedCallback) {
         m_onSessionEndedCallback();
     }
@@ -24,22 +60,19 @@ NearObjectCliUwbSessionEventCallbacks::OnSessionEnded(::uwb::UwbSession* session
 void
 NearObjectCliUwbSessionEventCallbacks::OnRangingStarted(::uwb::UwbSession* session)
 {
-    std::cout << "Session with id="
-              << std::hex << std::setw(8) << std::setfill('0') << std::showbase << std::internal << session->GetId() << ": Ranging Started" << std::endl;
+    std::cout << LogPrefix(session->GetId()) << "Ranging Started" << std::endl;
 }
 
 void
 NearObjectCliUwbSessionEventCallbacks::OnRangingStopped(::uwb::UwbSession* session)
 {
-    std::cout << "Session with id="
-              << std::hex << std::setw(8) << std::setfill('0') << std::showbase << std::internal << session->GetId() << ": Ranging Stopped" << std::endl;
+    std::cout << LogPrefix(session->GetId()) << "Ranging Stopped" << std::endl;
 }
 
 void
 NearObjectCliUwbSessionEventCallbacks::OnPeerPropertiesChanged(::uwb::UwbSession* session, const std::vector<::uwb::UwbPeer> peersChanged)
 {
-    std::cout << "Session with id="
-              << std::hex << std::setw(8) << std::setfill('0') << std::showbase << std::internal << session->GetId() << ": Peer Properties Changed" << std::endl;
+    std::cout << LogPrefix(session->GetId()) << "Peer Properties Changed" << std::endl;
 
     for (const auto& peer : peersChanged) {
         std::cout << " > " << peer << std::endl;
@@ -49,8 +82,7 @@ NearObjectCliUwbSessionEventCallbacks::OnPeerPropertiesChanged(::uwb::UwbSession
 void
 NearObjectCliUwbSessionEventCallbacks::OnSessionMembershipChanged(::uwb::UwbSession* session, const std::vector<::uwb::UwbPeer> peersAdded, const std::vector<::uwb::UwbPeer> peersRemoved)
 {
-    std::cout << "Session with id="
-              << std::hex << std::setw(8) << std::setfill('0') << std::showbase << std::internal << session->GetId() << ": Membership Changed" << std::endl;
+    std::cout << LogPrefix(session->GetId()) << "Membership Changed" << std::endl;
 
     for (const auto& peer : peersAdded) {
         std::cout << "+" << peer.GetAddress() << std::endl;
