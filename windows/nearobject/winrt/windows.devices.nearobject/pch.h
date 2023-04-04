447627a80fb9a74c86e5f7d3a4b39bf4ffc045b6
Andrew Beltrano <abeltrano@gmail.com>
2023-04-04 13:31:15 -0600

    Document conditions for use of PCH file.

--- windows/nearobject/winrt/windows.devices.nearobject/pch.h
+++ windows/nearobject/winrt/windows.devices.nearobject/pch.h
@@ -1,4 +1,13 @@
-﻿#pragma once
+﻿/**
+ * @brief Pre-compiled header. 
+ * 
+ * DO NOT ADD NON-WINRT GENERATED HEADERS HERE.
+ * 
+ * This should *only* be used to include the extremely large generated C++/WinRT
+ * headers, which can be several MBs in size. 
+ */
+#pragma once
+
 #include <unknwn.h>
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
