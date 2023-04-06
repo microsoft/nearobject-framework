a21d6ddb00f9e4f0a39e4fb23e490cb1f06ec036
Andrew Beltrano <abeltrano@gmail.com>
2023-04-06 06:41:35 -0600

    Add NearObject construction.

--- windows/nearobject/winrt/windows.devices.nearobject.service.app/main.cpp
+++ windows/nearobject/winrt/windows.devices.nearobject.service.app/main.cpp
@@ -1,16 +1,19 @@
 ﻿#include "pch.h"
+#include "winrt/Windows.Devices.NearObject.h"
 #include "winrt/Windows.Devices.NearObject.Service.h"
 
 #include <iostream>
 
 using namespace winrt;
 using namespace Windows::Foundation;
+using namespace Windows::Devices::NearObject;
 using namespace Windows::Devices::NearObject::Service;
 
 int main()
 {
     init_apartment();
-    NearObjectService nearObjectService(nullptr);
+    NearObject nearObject;
+    NearObjectService nearObjectService;
     std::cout << "Near Object Service Application started" << std::endl;
 
     return 0;
