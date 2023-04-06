760b7c598a3a43b78053c52f838569c711b218ac
Andrew Beltrano <abeltrano@gmail.com>
2023-04-05 20:42:44 -0600

    Add service exe project.

---
+++ windows/nearobject/winrt/windows.devices.nearobject.service.app/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "winrt/Windows.Devices.NearObject.Service.h"
+
+#include <iostream>
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::Devices::NearObject::Service;
+
+int main()
+{
+    init_apartment();
+    NearObjectService nearObjectService(nullptr);
+    std::cout << "Near Object Service Application started" << std::endl;
+
+    return 0;
+}
