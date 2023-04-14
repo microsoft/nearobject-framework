1aaf8e001d124e2b7dd17f812dbf1868459d87ff
Shane Guan <shaneguan@microsoft.com>
2023-04-14 14:08:57 -0700

    update docs

--- lib/uwb/include/uwb/UwbRegisteredCallbacks.hxx
+++ lib/uwb/include/uwb/UwbRegisteredCallbacks.hxx
@@ -13,31 +13,78 @@ namespace uwb
 {
 namespace UwbRegisteredSessionEventCallbackTypes
 {
+/**
+ * @brief Invoked when the session is ended.
+ *
+ * @param reason The reason the session ended.
+ */
 using OnSessionEnded = std::function<void(::uwb::UwbSessionEndReason reason)>;
+
+/**
+ * @brief Invoked when active ranging starts.
+ *
+ */
 using OnRangingStarted = std::function<void()>;
+
+/**
+ * @brief Invoked when active ranging stops.
+ *
+ */
 using OnRangingStopped = std::function<void()>;
+
+/**
+ * @brief Invoked when the properties of a peer involved in the session
+ * changes. This includes the spatial properties of the peer(s).
+ *
+ * @param peersChanged A list of peers whose properties changed.
+ */
 using OnPeerPropertiesChanged = std::function<void(const std::vector<UwbPeer> peersChanged)>;
+
+/**
+ * @brief Invoked when membership of one or more near peers involved in
+ * the session is changed. This can occur when peer members are either
+ * added to or removed from the session.
+ *
+ * @param peersAdded A list of peers that were added to the session.
+ * @param peersRemoved A list of peers that were removed from the session.
+ */
 using OnSessionMembershipChanged = std::function<void(const std::vector<UwbPeer> peersAdded, const std::vector<UwbPeer> peersRemoved)>;
 }; // namespace UwbRegisteredSessionEventCallbackTypes
 
 namespace UwbRegisteredDeviceEventCallbackTypes
 {
+/**
+ * @brief Invoked when a generic error occurs.
+ *
+ * @param status The generic error that occurred.
+ */
 using OnStatusChanged = std::function<void(::uwb::protocol::fira::UwbStatus)>;
+
+/**
+ * @brief Invoked when the device status changes.
+ *
+ * @param statusDevice
+ */
 using OnDeviceStatusChanged = std::function<void(::uwb::protocol::fira::UwbStatusDevice)>;
+
+/**
+ * @brief Invoked when the status of a session changes.
+ *
+ * @param statusSession The new status of the session.
+ */
 using OnSessionStatusChanged = std::function<void(::uwb::protocol::fira::UwbSessionStatus)>;
 }; // namespace UwbRegisteredDeviceEventCallbackTypes
 
 /**
  * @brief Interface for receiving events from a UwbSession. This is the primary
  * method to receive information from near peers.
- *
+ * IMPORTANT The client should save somewhere the shared_ptr to the callbacks that they register.
  */
 struct UwbRegisteredSessionEventCallbacks
 {
     /**
      * @brief Invoked when the session is ended.
      *
-     * @param session The session for which the event occurred.
      * @param reason The reason the session ended.
      */
     std::weak_ptr<UwbRegisteredSessionEventCallbackTypes::OnSessionEnded> OnSessionEnded;
@@ -45,14 +92,12 @@ struct UwbRegisteredSessionEventCallbacks
     /**
      * @brief Invoked when active ranging starts.
      *
-     * @param session The session for which the event occurred.
      */
     std::weak_ptr<UwbRegisteredSessionEventCallbackTypes::OnRangingStarted> OnRangingStarted;
 
     /**
      * @brief Invoked when active ranging stops.
      *
-     * @param session The session for which the event occurred.
      */
     std::weak_ptr<UwbRegisteredSessionEventCallbackTypes::OnRangingStopped> OnRangingStopped;
 
@@ -60,7 +105,6 @@ struct UwbRegisteredSessionEventCallbacks
      * @brief Invoked when the properties of a peer involved in the session
      * changes. This includes the spatial properties of the peer(s).
      *
-     * @param session The session for which the event occurred.
      * @param peersChanged A list of peers whose properties changed.
      */
     std::weak_ptr<UwbRegisteredSessionEventCallbackTypes::OnPeerPropertiesChanged> OnPeerPropertiesChanged;
@@ -70,7 +114,6 @@ struct UwbRegisteredSessionEventCallbacks
      * the session is changed. This can occur when peer members are either
      * added to or removed from the session.
      *
-     * @param session The session for which the event occurred.
      * @param peersAdded A list of peers that were added to the session.
      * @param peersRemoved A list of peers that were removed from the session.
      */
