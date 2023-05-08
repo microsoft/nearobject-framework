c3d0e99d24cec534ea1d1758c9b6b0cfc0c844c4
Andrew Beltrano <abeltrano@gmail.com>
2023-05-08 13:52:32 -0600

    Add UwbSessionDataPage and associated ViewModel.

---
+++ windows/tools/OobSimulator/UwbSessionDataPage.xaml.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "UwbSessionDataPage.xaml.h"
+#if __has_include("UwbSessionDataPage.g.cpp")
+#include "UwbSessionDataPage.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+namespace winrt::OobSimulator::implementation
+{
+    UwbSessionDataPage::UwbSessionDataPage()
+    {
+        InitializeComponent();
+    }
+
+    winrt::OobSimulator::UwbSessionDataPageViewModel UwbSessionDataPage::ViewModel()
+    {
+        return m_viewModel;
+    }
+}
---
+++ windows/tools/OobSimulator/UwbSessionDataPageViewModel.h
@@ -0,0 +1,49 @@
+﻿
+#ifndef UWB_SESSION_DATA_PAGE_VIEW_MODEL_H
+#define UWB_SESSION_DATA_PAGE_VIEW_MODEL_H
+
+// clang-format off
+#include "UwbSessionDataPageViewModel.g.h"
+#include "BindableBase.h"
+
+#include "DelegateCommand.h"
+#include "UwbSessionData.h"
+// clang-format on
+#include <uwb/protocols/fira/FiraDevice.hxx>
+
+namespace winrt::OobSimulator::implementation
+{
+struct UwbSessionDataPageViewModel : UwbSessionDataPageViewModelT<UwbSessionDataPageViewModel, OobSimulator::implementation::BindableBase>
+{
+    UwbSessionDataPageViewModel();
+
+    winrt::OobSimulator::UwbSessionData
+    UwbSessionData();
+    
+    winrt::Microsoft::UI::Xaml::Input::ICommand
+    SetUwbSessionDataCommand();
+
+private:
+    winrt::Windows::Foundation::IAsyncAction
+    SetUwbSessionData();
+
+    uwb::protocol::fira::DeviceRole
+    ConvertDeviceRole();
+
+    uwb::protocol::fira::MultiNodeMode
+    ConvertMultiNodeMode();
+
+private:
+    winrt::OobSimulator::UwbSessionData m_uwbSessionData{ nullptr };
+    OobSimulator::DelegateCommand m_setUwbSessionData{ nullptr };
+};
+} // winrt::OobSimulator::implementation
+
+namespace winrt::OobSimulator::factory_implementation
+{
+struct UwbSessionDataPageViewModel : UwbSessionDataPageViewModelT<UwbSessionDataPageViewModel, implementation::UwbSessionDataPageViewModel>
+{
+};
+} // winrt::OobSimulator::factory_implementation
+
+#endif // UWB_SESSION_DATA_PAGE_VIEW_MODEL_H
