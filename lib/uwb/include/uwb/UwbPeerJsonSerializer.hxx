3a273b6b862b6f8c23f31629ea84c31208e8f52d
Andrew Beltrano <abeltrano@gmail.com>
2023-06-08 21:13:44 -0600

    Remove commented out code.

--- lib/uwb/include/uwb/UwbPeerJsonSerializer.hxx
+++ lib/uwb/include/uwb/UwbPeerJsonSerializer.hxx
@@ -30,24 +30,6 @@ from_json(const nlohmann::json& json, UwbPeer& uwbPeer);
 
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(UwbPeerSpatialProperties, Distance, AngleAzimuth, AngleElevation, Elevation, AngleAzimuthFom, AngleElevationFom, ElevationFom)
 
-// /**
-//  * @brief 
-//  * 
-//  * @param json 
-//  * @param uwbPeerSpatialProperties 
-//  */
-// void
-// to_json(nlohmann::json& json, const UwbPeerSpatialProperties& uwbPeerSpatialProperties);
-
-// /**
-//  * @brief 
-//  * 
-//  * @param json 
-//  * @param uwbPeerSpatialProperties 
-//  */
-// void
-// from_json(const nlohmann::json& json, UwbPeerSpatialProperties& uwbPeerSpatialProperties);
-
 } // namespace uwb
 
 #endif // UWB_PEER_JSON_SERIALIZER_HXX
